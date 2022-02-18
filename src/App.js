2153b3d8bd70ca9daaed8223d21992b8fe524e4c
unknown <esteban.abella1@gmail .com>
2022-02-18 09:49:23 -0300

    creacion de componente screenDashboardPage

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import SchoolCampaign from './Campaigns/School/SchoolCampaign';
 import ToysCampaign from './Campaigns/Toys/ToysCampaign';
 import MembersForm from './Components/Members/MembersForm';
 import ProjectsForm from './Components/Projects/ProjectsForm';
+import ScreenDashboardPage from './Components/ScreenDashboard/ScreenDashboardPage'
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
           <Route path="/create-project" component={ProjectsForm} />
           <Route path="/school-campaign" component={SchoolCampaign} />
           <Route path="/toys-campaign" component={ToysCampaign} />
+          <Route path="/backoffice" component={ScreenDashboardPage} />
         </Switch>
       </BrowserRouter>
     <div className="App">
---
+++ src/Components/ScreenDashboard/ScreenDashboardPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./stylesScreenDashboardPage.css"
+
+function ScreenDashboardPage(){
+    return(
+        <div className="screenDashboardPage">
+            <h2>Bienvenido</h2>
+        </div>
+    )
+}
+
+export default ScreenDashboardPage
