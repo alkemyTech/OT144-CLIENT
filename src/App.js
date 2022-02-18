cfba957dd1e9d5d17f93a4c8109059095f6a5fd9
Mariel Romeo <marielceciliaromeo@hotmail.com>
2022-02-18 10:04:12 -0300

    [ADD] Organization edit form

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import SchoolCampaign from './Campaigns/School/SchoolCampaign';
 import ToysCampaign from './Campaigns/Toys/ToysCampaign';
 import MembersForm from './Components/Members/MembersForm';
 import ProjectsForm from './Components/Projects/ProjectsForm';
+import OrganizationEditForm from './Components/Home/BackOffice/OrganizationEditForm';
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
           <Route path="/create-category" component={CategoriesForm} />
           <Route path="/create-news" component={NewsForm} />
           <Route path="/backoffice/create-slide" component={SlidesForm} />
+          <Route path="/backoffice/organization/edit" component={OrganizationEditForm} />
           <Route path="/create-testimonials" component={TestimonialForm} />
           <Route path="/create-user" component={UserForm} />
           <Route path="/create-member" component={MembersForm} />
