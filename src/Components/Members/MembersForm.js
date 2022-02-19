abc066190e9851d01f884fc66512edf6015f1a4a
galo <67323030+gfalotico@users.noreply.github.com>
2022-02-19 20:56:14 -0300

    Logical structure of the component
    No styling

--- src/Components/Members/MembersForm.js
+++ src/Components/Members/MembersForm.js
@@ -1,32 +1,155 @@
-import React, { useState } from 'react';
-import '../FormStyles.css';
+import React, { useState } from "react";
+import "../FormStyles.css";
+import { Formik } from "formik";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
 const MembersForm = () => {
-  const [initialValues, setInitialValues] = useState({
-    name: '',
-    description: ''
-  })
+  const [image, setImage] = useState("");
+  const formValues = [
+    {
+      type: "file",
+      name: "image",
+      placeholder: "Enter the image",
+      validate: (value) => {
+        if (!value) {
+          return "Image is required";
+        }
+      },
+      onChange: (e, handleChange) => {
+        setImage(e.target.files[0]);
+        handleChange(e);
+      },
+    },
+    {
+      type: "text",
+      name: "name",
+      placeholder: "Enter name",
+      validate: (value) => {
+        if (!value) {
+          return "Name is required";
+        }
+        if (value.length < 4) {
+          return "Name must be at least 4 characters";
+        }
+      },
+    },
+    {
+      type: "text",
+      name: "instagram",
+      placeholder: "Enter instagram",
+      validate: (value) => {
+        if (!value) {
+          return "Instagram is required";
+        }
+      },
+    },
+    {
+      type: "text",
+      name: "twitter",
+      placeholder: "Enter twitter",
+      validate: (value) => {
+        if (!value) {
+          return "Twitter is required";
+        }
+      },
+    },
+    {
+      type: "text",
+      name: "facebook",
+      placeholder: "Enter facebook",
+      validate: (value) => {
+        if (!value) {
+          return "Facebook is required";
+        }
+      },
+    },
+  ];
 
-  const handleChange = (e) => {
-    if(e.target.name === 'name'){
-      setInitialValues({...initialValues, name: e.target.value})
-    } if(e.target.name === 'description'){
-      setInitialValues({...initialValues, description: e.target.value})
+  const validate = (values) => {
+    const errors = {};
+    formValues.forEach(({ validate, name }) => {
+      if (validate(values[name])) {
+        errors[name] = validate(values[name]);
+      }
+    });
+
+    if (
+      !errors.password_confirmation &&
+      values.password !== values.password_confirmation
+    ) {
+      errors.password_confirmation =
+        "Password Confirmation must be the same as password";
     }
-  }
+    return errors;
+  };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(initialValues);
-  }
+  const handleSubmit = (values, { setSubmitting }) => {
+    const newValues = {
+      ...values,
+      ...image,
+      instagram: "https://www.instagram.com/" + values.instagram,
+      twitter: "https://www.twitter.com/" + values.twitter,
+      facebook: "https://www.facebook.com/" + values.facebook,
+    };
+    console.info(newValues);
+    setSubmitting(false);
+    localStorage.setItem("token", "tokenValueExample");
+  };
 
   return (
-    <form className="form-container" onSubmit={handleSubmit}>
-      <input className="input-field" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Name"></input>
-      <input className="input-field" type="text" name="description" value={initialValues.description} onChange={handleChange} placeholder="Write some description"></input>
-      <button className="submit-btn" type="submit">Send</button>
-    </form>
+    <Formik
+      initialValues={{}}
+      validate={validate}
+      onSubmit={(values, helpers) => handleSubmit(values, helpers)}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleSubmit,
+        isSubmitting,
+      }) => (
+        <form className="form-container" onSubmit={handleSubmit}>
+          {formValues.map((item) => (
+            <React.Fragment key={item.name}>
+              <input
+                type={item.type}
+                name={item.name}
+                onChange={(e) =>
+                  item.onChange
+                    ? item.onChange(e, handleChange)
+                    : handleChange(e)
+                }
+                value={values[item.name]}
+                placeholder={item.placeholder}
+                className="input-field"
+              />
+              {errors[item.name] && touched[item.name] && errors[item.name]}
+            </React.Fragment>
+          ))}
+          <CKEditor
+            editor={ClassicEditor}
+            data="<p>¡Escribi una descripcion!</p>"
+            onChange={(event, editor) => {
+              const data = editor.getData();
+              let e = {
+                target: {
+                  value: data,
+                  name: "description",
+                },
+              };
+              handleChange(e);
+            }}
+          />
+          <button type="submit" className="submit-btn" disabled={isSubmitting}>
+            Submit
+          </button>
+        </form>
+      )}
+    </Formik>
   );
-}
- 
+};
+
 export default MembersForm;
