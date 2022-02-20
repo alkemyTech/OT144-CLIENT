31b2267c88a5f3b31d2301b97562a5dd5290ab77
Mariel Romeo <marielceciliaromeo@hotmail.com>
2022-02-20 12:35:53 -0300

    [FIX] code review: delete commented code and change global css to FormStyles.css

--- src/Components/Home/BackOffice/OrganizationEditForm.js
+++ src/Components/Home/BackOffice/OrganizationEditForm.js
@@ -37,20 +37,6 @@ export default function OrganizationEditForm() {
     const handleChangeLogo = (props) => {
         const file = document.querySelector('input[type=file]').files[0];
         props.values.logo= file
-
-        /*
-        GUARDE LA IMAGEN COMO FILE. CON ESTO SE PUEDE LEER Y CARGAR AL SRC DE LA IMG.
-        const reader  = new FileReader();
-        const preview = document.querySelector('img');
-        reader.onloadend=()=>{
-            preview.src = reader.result;
-        }
-
-        if(file){
-            reader.readAsDataURL(file)
-        }else{
-            preview.src=""
-        }*/
     }
 
     return (
@@ -97,33 +83,3 @@ export default function OrganizationEditForm() {
         </Formik>
     )
 }
-{/*
-        <div className="form-container organization">
-            <form onSubmit={handleSubmit}>
-                <input type="text" name="name" value={values.name} className="input-field" onChange={handleChangeName} placeholder="Nombre de la organización*" required />
-                <div className="logo-container">
-                    <label>Logo: </label>
-                    <input type="file" name="logo" className="file-field" onChange={handleChangeLogo} accept=".png, .jpg" required />
-                </div>
-                <div>
-                    <CKEditor
-                        editor={ClassicEditor}
-                        data={values.shortDescription}
-                        onFocus={(event, editor) => {
-                            console.log(editor.data);
-                            editor.data.set("")
-                        }}
-                        onChange={(event, editor) => {
-                            const data = editor.getData();
-                            setValues({ ...values, shortDescription: data })
-                        }}
-                    />
-                </div>
-                <textarea name="longDescription" value={values.longDescription} onChange={handleChangeLongDescription} placeholder="Ingresá una descripción detallada*" className="textarea-field" />
-                <textarea name="socialNetworkLinks" value={values.socialNetworkLinks} onChange={handleChangeSocialNetworks} placeholder="Ingresá url de tus redes sociales (separadas por coma)*" className="textarea-field" />
-                <button className="submit-btn" type="submit">Enviar</button>
-                <div>{values.error}</div>
-            </form>
-        </div>
-                    </>*/}
-
