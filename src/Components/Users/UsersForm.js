427a64f5cd23b2a94a579f738955e68d95d705f2
juliandebra <julian.debra25@gmail.com>
2022-02-22 19:50:53 -0300

    syntax corrected

--- src/Components/Users/UsersForm.js
+++ src/Components/Users/UsersForm.js
@@ -4,7 +4,7 @@ import ImagePreview from './ImagePreview';
 import { Formik, Form, Field } from 'formik';
 import { validateUsername, validateEmail, validatePass, validateRole } from './ValidateFunctions';
 import '../FormStyles.css';
-const SUPPORTED_FORMATS = ["image/x-png", "image/x-jpg"];
+const SUPPORTED_FORMATS = ['image/png', 'image/jpg'];
 
 const UserForm = () => {
 
@@ -68,29 +68,24 @@ const UserForm = () => {
             {({ handleChange, values, setFieldValue, errors, touched }) => (
                 <Form className='form-container'>
                     
-                    {/* Name Field */}
-                    <Field className="input-field" type="text" name="name" value={values.name || ''} 
+                    <Field className="input-field" type="text" name="name" value={values.name} 
                     onChange={handleChange} placeholder="Name" validate={validateUsername} />
                     {errors.name && touched.name && <div className="input-feedback">{errors.name}</div>}
                     
-                    {/* Email Field */}
-                    <Field className="input-field" type="text" name="email" value={values.email || ''} 
+                    <Field className="input-field" type="text" name="email" value={values.email} 
                     onChange={handleChange} placeholder="Email" validate={validateEmail}/>
                     {errors.email && touched.email && <div className="input-feedback">{errors.email}</div>}
                     
-                    {/* Password Field */}
-                    <Field className="input-field" type="password" name="password" value={values.password || ''} 
+                    <Field className="input-field" type="password" name="password" value={values.password} 
                     onChange={handleChange} placeholder="Password" validate={validatePass}/>
                     {errors.password && touched.password && <div className="input-feedback">{errors.password}</div>}
                     
-                    {/* Image upload and preview */}
                     {values.file && <ImagePreview file={values.file} />}
-                    <Field className='input-field' type='file' accept={SUPPORTED_FORMATS} value={values.profilePhoto} 
+                    <Field className='input-field' type='file' accept={[SUPPORTED_FORMATS]} value={values.profilePhoto} 
                     onChange={(event) => setFieldValue("file", event.target.files[0])} />                   
                     {errors.file && touched.file && <div className="input-feedback">{errors.file}</div>}
                     
-                    {/* Role select */}
-                    <Field component="select" name="roleId" id="roleId" className="input-field" value={values.roleId || ''} 
+                    <Field component="select" name="roleId" id="roleId" className="input-field" value={values.roleId} 
                     onChange={handleChange} validate={validateRole}>
                                     <option value="" disabled >Seleccione un Rol</option>
                                     <option value="1">Admin</option>
