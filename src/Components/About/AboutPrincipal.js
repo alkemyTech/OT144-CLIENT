e89700985199621356da94b75ce4b32e6cc075b0
Mariel Romeo <marielceciliaromeo@hotmail.com>
2022-02-22 19:01:06 -0300

    [FIX] text about us and main tag

--- src/Components/About/AboutPrincipal.js
+++ src/Components/About/AboutPrincipal.js
@@ -1,11 +1,11 @@
 import AboutUs from "./AboutUs";
 
 export default function AboutPrincipal(){
-    const principalText= "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste accusamus nostrum explicabo recusandae vitae incidunt atque magnam illo modi repellendus, quasi sapiente earum excepturi similique iusto ipsa accusantium ab vero."
+    const principalText= "Desde 1997 en Somos Más trabajamos con los chicos y chicas, mamás y papás, abuelos y vecinos del barrio La Cava generando procesos de crecimiento y de inserción social. Uniendo las manos de todas las familias, las que viven en el barrio y las que viven fuera de él, es que podemos pensar, crear y garantizar estos procesos. ﻿ Somos una asociación civil sin fines de lucro que se creó en 1997 con la intención de dar alimento a las familias del barrio. Con el tiempo fuimos involucrándonos con la comunidad y agrandando y mejorando nuestra capacidad de trabajo. Hoy somos un centro comunitario que acompaña a más de 700 personas a través de las áreas de: Educación, deportes, primera infancia, salud, alimentación y trabajo social."
 
     return(
-        <div className="aboutPrincipal-container">
+        <main className="aboutPrincipal-container">
             <AboutUs text={principalText}/>
-        </div>
+        </main>
     )
 }
