4f41eead07b9fddc06ef433fb87e41aa66a88a68
Marlon Gómez <marlonjulian15@gmail.com>
2022-02-22 12:27:09 -0500

    Create CreateNews component

---
+++ src/Components/News/CreateNews.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NewsForm from "./NewsForm";
+import "./News.css";
+
+const CreateNews = () => {
+  return (
+    <div className="create-news-container">
+      <h1>Crear Novedad</h1>
+      <NewsForm />
+    </div>
+  );
+};
+
+export default CreateNews;
