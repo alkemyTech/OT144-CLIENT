2ecc3547422c63a8002f9c8e83c891ece2044e23
cesar londoño <slac_@hotmail.com>
2022-02-22 00:28:44 -0300

    component actividades route render

--- src/Components/Slides/SlidesForm.js
+++ src/Components/Slides/SlidesForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Actividades from '../Activities/Actividades';
 import '../FormStyles.css';
 import TitleComponent from '../title/TitleComponent';
 
@@ -22,12 +23,14 @@ const SlidesForm = () => {
     }
 
     return (
+        <>
         
         <form className="form-container" onSubmit={handleSubmit}>
             <input className="input-field" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Slide Title"></input>
             <input className="input-field" type="text" name="description" value={initialValues.description} onChange={handleChange} placeholder="Write the description"></input>
             <button className="submit-btn" type="submit">Send</button>
         </form>
+        </>
         
     );
 }
