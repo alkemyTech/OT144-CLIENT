50e7fee3fab92a4a324c674f91ab106ea0cbb5ed
juliandebra <julian.debra25@gmail.com>
2022-02-22 20:04:25 -0300

    Update react-router-dom to v6

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import ActivitiesForm from './Components/Activities/ActivitiesForm';
 import CategoriesForm from './Components/Categories/CategoriesForm';
 import NewsForm from './Components/News/NewsForm';
@@ -18,19 +18,19 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Switch>
-          {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
-          <Route path="/create-activity" component={ActivitiesForm} />
-          <Route path="/create-category" component={CategoriesForm} />
-          <Route path="/create-news" component={NewsForm} />
-          <Route path="/backoffice/create-slide" component={SlidesForm} />
-          <Route path="/create-testimonials" component={TestimonialForm} />
-          <Route path="/create-user" component={UserForm} />
-          <Route path="/create-member" component={MembersForm} />
-          <Route path="/create-project" component={ProjectsForm} />
-          <Route path="/school-campaign" component={SchoolCampaign} />
-          <Route path="/toys-campaign" component={ToysCampaign} />
-        </Switch>
+        <Routes>
+          {/* <Route path="/" exact element={} />           Esta ruta debe ser para el Home */}
+          <Route path="/create-activity" element={ActivitiesForm} />
+          <Route path="/create-category" element={CategoriesForm} />
+          <Route path="/create-news" element={NewsForm} />
+          <Route path="/backoffice/create-slide" element={SlidesForm} />
+          <Route path="/create-testimonials" element={TestimonialForm} />
+          <Route path="/create-user" element={UserForm} />
+          <Route path="/create-member" element={MembersForm} />
+          <Route path="/create-project" element={ProjectsForm} />
+          <Route path="/school-campaign" element={SchoolCampaign} />
+          <Route path="/toys-campaign" element={ToysCampaign} />
+        </Routes>
       </BrowserRouter>
     <div className="App">
       <header className="App-header">
