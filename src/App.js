b3f65323a0a08b3a402ac5dec080e4c937bf6436
juliandebra <julian.debra25@gmail.com>
2022-02-21 22:27:23 -0300

    Home form route changed in app

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
           <Route path="/create-activity" component={ActivitiesForm} />
           <Route path="/create-category" component={CategoriesForm} />
           <Route path="/create-news" component={NewsForm} />
-          <Route path="/backoffice/organization" component={HomeForm} />
+          <Route path="/backoffice/organization/edit-home" component={HomeForm} />
           <Route path="/backoffice/create-slide" component={SlidesForm} />
           <Route path="/create-testimonials" component={TestimonialForm} />
           <Route path="/create-user" component={UserForm} />
