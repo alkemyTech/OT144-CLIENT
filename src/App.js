3210afa6b1f3f160944889f0301b66c18445b254
galo <67323030+gfalotico@users.noreply.github.com>
2022-02-21 21:03:23 -0300

    Add route

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
           <Route path="/create-category" component={CategoriesForm} />
           <Route path="/create-news" component={NewsForm} />
           <Route path="/backoffice/create-slide" component={SlidesForm} />
+          <Route path="/backoffice/members/edit" component={MembersForm} />
           <Route path="/create-testimonials" component={TestimonialForm} />
           <Route path="/create-user" component={UserForm} />
           <Route path="/create-member" component={MembersForm} />
