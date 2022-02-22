cc8d9889d8aafb6aaa3472f841be7a9b15c75569
Mariel Romeo <marielceciliaromeo@hotmail.com>
2022-02-22 09:46:21 -0300

    feat: org form
    * [ADD] Organization edit form
    
    * [ADD] event onChange organization edit form
    
    * [ADD] handleChangeLogo - fileReader function
    
    * [ADD] shortDescription to values
    
    * [FIX] change language from english to spanish on the inputs
    
    * [ADD] socialNetworkLinks
    
    * [ADD] social network url validation
    
    * [ADD] css
    
    * [ADD] formik
    
    * [DELETE] console.log
    
    * [FIX] regex social networks url
    
    * [FIX] code review: delete commented code and change global css to FormStyles.css
    
    * [ADD] organizationEditForm css
    
    Co-authored-by: Federico Miranda <federico.mirandaa@gmail.com>

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import SchoolCampaign from './Campaigns/School/SchoolCampaign';
 import ToysCampaign from './Campaigns/Toys/ToysCampaign';
 import MembersForm from './Components/Members/MembersForm';
 import ProjectsForm from './Components/Projects/ProjectsForm';
+import OrganizationEditForm from './Components/Home/BackOffice/OrganizationEditForm';
 import ScreenDashboardPage from './Components/ScreenDashboard/ScreenDashboardPage'
 import HomePage from './Components/Home/HomePage'
 import Register from './Components/Auth/RegisterForm';
@@ -29,6 +30,7 @@ function App() {
           <Route path="/create-news" component={NewsForm} />
           <Route path="/backoffice/organization/edit-home" component={HomeForm} />
           <Route path="/backoffice/create-slide" component={SlidesForm} />
+          <Route path="/backoffice/organization/edit" component={OrganizationEditForm} />
           <Route path="/create-testimonials" component={TestimonialForm} />
           <Route path="/create-user" component={UserForm} />
           <Route path="/create-member" component={MembersForm} />
