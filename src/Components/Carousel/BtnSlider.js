2700c99ea28999fcdc8cebac7344fd3e68a4219e
Braifz <braian94lp@gmail.com>
2022-02-22 14:55:33 -0300

    [FIX] add alt attribute to<img/>

--- src/Components/Carousel/BtnSlider.js
+++ src/Components/Carousel/BtnSlider.js
@@ -9,7 +9,10 @@ export default function BtnSlider({ direction, moveSlide }) {
       onClick={moveSlide}
       className={direction === "next" ? "btn-slide next" : "btn-slide prev"}
     >
-      <img src={direction === "next" ? rightArrow : leftArrow} />
+      <img
+        src={direction === "next" ? rightArrow : leftArrow}
+        alt="button-slider"
+      />
     </button>
   );
 }
