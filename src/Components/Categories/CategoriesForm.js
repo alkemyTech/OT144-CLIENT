e9d37ad11e1414bcc2acad67265c797edcc45364
unknown <esteban.abella1@gmail .com>
2022-02-23 11:39:14 -0300

    Task/se agregaron estilos

--- src/Components/Categories/CategoriesForm.js
+++ src/Components/Categories/CategoriesForm.js
@@ -3,6 +3,7 @@ import '../FormStyles.css';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import axios from "axios";
+import './stylesCategoriesForm.css'
 
 const CategoriesForm = (props) => {
 
@@ -139,9 +140,9 @@ const CategoriesForm = (props) => {
 
 
     return (
-        <>
-        <form className="form-container" onSubmit={handleSubmit}>
-            <input className="input-field" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Title"></input>
+        <section className="categoriesForm">
+        <form className="form-container form-container-Category" onSubmit={handleSubmit}>
+            <input className="input-field input-field-Category" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Título"></input>
             {msjFormIncomplete && 
                 <p className="msjIncompleteForm">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-circle" viewBox="0 0 16 16">
@@ -151,7 +152,7 @@ const CategoriesForm = (props) => {
                 Campo obligatorio y debe contener al menos 4 caracteres</p>
             }
 
-            <CKEditor editor={ ClassicEditor } onChange={handleCkreditor} data={categories?.description ? categories.description : '' }/>
+            <CKEditor className='ckeditorForm' editor={ ClassicEditor } onChange={handleCkreditor} data={categories?.description ? categories.description : '' }/>
             {msjFormIncompleteDes && 
                 <p className="msjIncompleteForm">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-circle" viewBox="0 0 16 16">
@@ -161,7 +162,7 @@ const CategoriesForm = (props) => {
                 Campo obligatorio</p>
             }
 
-            <input className="input-field" type="file" ref={initialValues.image} name='image' onChange={()=>getBase64(initialValues.image?.current?.files[0])}></input>
+            <input className="input-field input-field-Category" type="file" ref={initialValues.image} name='image' onChange={()=>getBase64(initialValues.image?.current?.files[0])}></input>
             {msjFormIncompleteImg && 
                 <p className="msjIncompleteForm">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-circle" viewBox="0 0 16 16">
@@ -172,26 +173,39 @@ const CategoriesForm = (props) => {
             }
 
             {loandingForm ?
-                <p>Loanding</p>
+                <p className="subtitle">Cargando...</p>
             :
                 <button className="submit-btn" type="submit" onClick={handleClick}>Send</button>
             }
 
             {messageErrorCategory &&
-                <div>
-                    <p>Error</p>
+                <div className="msjErrorCategory">
+                    <p className="subtitle">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" className="bi bi-x-circle" viewBox="0 0 16 16">
+                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                    </svg>
+                    Error</p>
                 </div>
             }
             {messageOkCategory &&
-                <div>
-                    <p>Tarea realizada</p>
+                <div className="msjOkCategory">
+                    <p className="subtitle">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" className="bi bi-check2-circle" viewBox="0 0 16 16">
+                        <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0z"/>
+                        <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l7-7z"/>
+                    </svg>
+                    Tarea realizada</p>
                 </div>
             }
         </form>
         {!categoriesBtn &&
-            <img src={categories?.image} alt='img category'></img>
+            <div className="containerImgCategory">
+                <h1 className="bodyTxt">Imagen actual</h1>
+                <img src={categories?.image} alt='img category'></img>
+            </div>
         }
-        </>
+        </section>
     );
 }
  
