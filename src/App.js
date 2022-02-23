6e59ef26e1bd7271f74886ddd82baadc9c085df7
Julián Debrabandere <64337359+juliandebra@users.noreply.github.com>
2022-02-22 23:10:14 -0300

    feat: organization data screen
    * Task: Org Data Screen
    
    * onChange function optimized
    
    Co-authored-by: Federico Miranda <federico.mirandaa@gmail.com>

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import ActivitiesForm from './Components/Activities/ActivitiesForm';
 import CategoriesForm from './Components/Categories/CategoriesForm';
 import NewsForm from './Components/News/NewsForm';
 import SlidesForm from './Components/Slides/SlidesForm';
+import DataScreen from './Components/Testimonials/DataScreen';
+import EditDataScreen from './Components/Testimonials/EditDataScreen';
 import TestimonialForm from './Components/Testimonials/TestimonialsForm';
 import UserForm from './Components/Users/UsersForm';
 import SchoolCampaign from './Campaigns/School/SchoolCampaign';
@@ -32,8 +34,9 @@ function App() {
           <Route path="/create-news" component={NewsForm} />
           <Route path="/backoffice/organization/edit-home" component={HomeForm} />
           <Route path="/backoffice/create-slide" component={SlidesForm} />
+          <Route path="/backoffice/organization" component={DataScreen}/>
+          <Route path="/backoffice/organization-edit" component={EditDataScreen}/>
           <Route path="/backoffice/members/edit" component={MembersForm} />
-          <Route path="/backoffice/organization/edit" component={OrganizationEditForm} />
           <Route path="/create-testimonials" component={TestimonialForm} />
           <Route path="/create-user" component={UserForm} />
           <Route path="/create-member" component={MembersForm} />
