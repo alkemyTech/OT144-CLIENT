4dbe03a52e2bd184ed4fda4f6a1d9e95684243f1
Braifz <braian94lp@gmail.com>
2022-02-24 18:04:09 -0300

    create component Member.js

---
+++ src/Components/About/ListMemberNGO/Member.js
@@ -0,0 +1,25 @@
+import "./Member.css";
+import ImageLinkedin from "../../../assets/linkedin.svg";
+import ImageFacebook from "../../../assets/facebook.svg";
+
+const Member = ({ member }) => {
+  return (
+    <div className="member-container">
+      <div className="member-img-name">
+        <img src={member.image} alt={member.name} />
+        <p>{member.name}</p>
+      </div>
+      <p className="description"> {member.description}</p>
+      <div className="social-media">
+        <a href={member.facebookURL}>
+          <img src={ImageFacebook} alt="facebook-icon" />
+        </a>
+        <a href={member.linkedinURL}>
+          <img src={ImageLinkedin} alt="linkedin-icon" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Member;
