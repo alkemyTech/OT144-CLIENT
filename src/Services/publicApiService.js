abecf7284fd4965e2f03bd220cdd7a6201991d3e
galo <67323030+gfalotico@users.noreply.github.com>
2022-02-24 14:35:55 -0300

    structure

--- src/Services/publicApiService.js
+++ src/Services/publicApiService.js
@@ -2,14 +2,19 @@ import axios from 'axios';
 
 const config = {
     headers: {
-        Group: 01                //Aqui va el ID del equipo!!
+        Group: "01"                //Aqui va el ID del equipo!!
     }
 }
 
-const Get = () => {
+export const Get = () => {
     axios.get('https://jsonplaceholder.typicode.com/users', config)
     .then(res => console.log(res))
     .catch(err => console.log(err))
 }
 
-export default Get
+export const postRequest = (url, data) => {
+    
+}
+
+
+
