0b87161cac9dd77008713312b22a52fdc537a211
Mariel Romeo <marielceciliaromeo@hotmail.com>
2022-02-24 21:01:19 -0300

    [ADD] get request

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -2,14 +2,34 @@ import axios from 'axios';
 
 const config = {
     headers: {
-        Group: 01                //Aqui va el ID del equipo!!
+        Group: 144
     }
 }
 
-const Get = () => {
-    axios.get('https://jsonplaceholder.typicode.com/users', config)
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
+export const getRequest =  async(url,id,getToken) =>{
+    try {
+        let response = await axios({
+            method: 'get',
+            url: `${url}/${id}`,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization':getToken(),
+                ...config.headers
+            }
+        })
+
+        return {
+            status: response.status,
+            data: response.data
+        }
+    }
+    catch (error) {
+        return {
+            status: error.response.status,
+            error: error.message,
+            data: error.response.data
+        }
+    }
 }
 
-export default Get
+
