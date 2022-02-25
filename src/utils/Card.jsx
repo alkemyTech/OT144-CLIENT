76ab685489cde73877ff2f7bb5d291b8d7be20bc
juliandebra <julian.debra25@gmail.com>
2022-02-25 19:07:23 -0300

    Width and height modified

--- src/utils/Card.jsx
+++ src/utils/Card.jsx
@@ -4,13 +4,13 @@ const Card = ({title, image, description}) => {
 
   return (
     <div className="wrapper">
-      <div className='card-container'>
+      <section className='card-container'>
         {image ? <img src={image} alt={title} /> : 
         <img src={'/images/SomosMas.png'} alt={title}/>}
         {title ? <h1>{title}</h1> : <h1>Titulo de prueba</h1>}
         {description ? <p>{description}</p> : 
         <p>Descripción de prueba. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error facilis commodi illum? asdasdasdasdasdas dasdasdas</p>}
-      </div>
+      </section>
     </div>
   )
 }
