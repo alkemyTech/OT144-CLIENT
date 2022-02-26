26302f886f5ad401387766aa81558101e4736eac
Marlon Gómez <marlonjulian15@gmail.com>
2022-02-25 19:08:37 -0500

    Change name of variables

--- src/Services/slides.js
+++ src/Services/slides.js
@@ -7,25 +7,25 @@ import {
 
 export const getSlides = (search = "", skip = "", limit = "") => {
   const endPoint = `/slides?search=${search}&skip=${skip}&limit=${limit}`;
-  const data = getRequest(endPoint);
+  const response = getRequest(endPoint);
 };
 
 export const getSlide = (id) => {
   const endPoint = `/slides/${id}`;
-  const data = getRequest(endPoint);
+  const response = getRequest(endPoint);
 };
 
 export const createSlide = (data) => {
   const endPoint = `/slides`;
-  const data = postRequest(endPoint, data);
+  const response = postRequest(endPoint, data);
 };
 
 export const updateSlide = (data) => {
   const endPoint = `/slides/${data.id}`;
-  const data = putRequest(endPoint, data);
+  const response = putRequest(endPoint, data);
 };
 
 export const deleteSlide = (id) => {
   const endPoint = `/slides/${id}`;
-  const data = deleteRequest(endPoint);
+  const response = deleteRequest(endPoint);
 };
