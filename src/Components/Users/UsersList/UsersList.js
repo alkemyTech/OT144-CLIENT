6ae19f750dd8e1022164d3ca526deb08c6ec581a
Marlon Gómez <marlonjulian15@gmail.com>
2022-02-26 09:28:46 -0500

    Create UsersList component

---
+++ src/Components/Users/UsersList/UsersList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import UsersTable from "./UsersTable";
+
+const UsersList = () => {
+  return (
+    <div>
+      <h1>UsersList</h1>
+      <UsersTable />
+    </div>
+  );
+};
+
+export default UsersList;
