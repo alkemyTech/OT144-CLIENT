9636982b7c7119ccf64faef7da75c07969919fee
cesar londoño <slac_@hotmail.com>
2022-02-27 19:05:02 -0300

    firts patch p

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { baseURL } from './Api';
 
 const config = {
     headers: {
@@ -12,4 +13,27 @@ const Get = () => {
     .catch(err => console.log(err))
 }
 
+export const patchApi = async( enpoint, id, data  ) => {
+    try {
+        const resp = await axios({
+            method: 'patch',
+            url: `${baseURL}${enpoint}/${id}`,
+            data:data,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization':getToken(),
+            }
+        })
+        return {
+            status: resp.status,
+            data: resp.data
+        }
+    } catch (error) {
+        return {
+            status: error.resp.status,
+            error: error.message,
+            data: error.resp.data
+        }
+    }
+}
 export default Get
