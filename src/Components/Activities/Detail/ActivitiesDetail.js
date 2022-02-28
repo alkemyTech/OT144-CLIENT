ae0e8c9187badd53fb80c492bb8fc2de9cdf7850
Mariel Romeo <marielceciliaromeo@hotmail.com>
2022-02-28 18:03:57 -0300

    [UPDATE] commit suggestions

--- src/Components/Activities/Detail/ActivitiesDetail.js
+++ src/Components/Activities/Detail/ActivitiesDetail.js
@@ -8,10 +8,23 @@ export default function ActivitiesDetail(){
     const { id } = useParams();
     /* En el estado activity se va a guardar la informacion de la actividad buscandola 
     mediante el id */
-    const [activity, setActivity] = useState({"id":1352,"name":"Actividad","slug":null,"description":"Descripcion","image":"http:\/\/ongapi.alkemy.org\/storage\/RVaOs4LmMs.jpeg","user_id":null,"category_id":null,"created_at":"2022-02-20T20:30:08.000000Z","updated_at":"2022-02-20T20:30:08.000000Z","deleted_at":null,"group_id":36})
+    const [activity, setActivity] = useState({
+        "id":1352,
+        "name":"Actividad",
+        "slug":null,
+        "description":"Descripcion",
+        "image":"http:\/\/ongapi.alkemy.org\/storage\/RVaOs4LmMs.jpeg",
+        "user_id":null,
+        "category_id":null,
+        "created_at":"2022-02-20T20:30:08.000000Z",
+        "updated_at":"2022-02-20T20:30:08.000000Z",
+        "deleted_at":null,
+        "group_id":36
+    })
+
     return(
-        <section className = "containerActivity">
-            <TitleComponent text = {activity.name} img = {activity.image}/>
+        <section className="containerActivity">
+            <TitleComponent text={activity.name} img={activity.image}/>
             <p>{activity.description}</p>
         </section>
     )
