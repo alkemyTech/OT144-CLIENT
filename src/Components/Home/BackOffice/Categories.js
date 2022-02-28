7de037ced517300416244449efc00e6f54358046
Mariel Romeo <marielceciliaromeo@hotmail.com>
2022-02-28 17:59:55 -0300

    [FIX] change file TableStyles.css´ name and add css responsive to Categories.js

--- src/Components/Home/BackOffice/Categories.js
+++ src/Components/Home/BackOffice/Categories.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom"
-import "./stylesTable.css"
+import "../../TableStyles.css"
 
 export default function Categories() {
 
@@ -10,40 +10,42 @@ export default function Categories() {
         { id: 3, name: 'Categoria-3', createdAt: '2022-01-31' }
     ]);
 
-    const handleClickUpdate = (id) => {}
+    const handleClickUpdate = (id) => { }
 
-    const handleClickDelete = (id) => {}
+    const handleClickDelete = (id) => { }
 
     return (
         <section className="sectionTable">
-            <table className="table">
-                <thead>
-                    <tr>
-                        <td>
-                            <Link to="/backoffice/categories/create" className="btnAddTable">Crear</Link>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>Nombre</th>
-                        <th>Creado</th>
-                        <th>Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {categories.map(category => {
-                        return (
-                            <tr key = {category.id}>
-                                <td>{category.name}</td>
-                                <td>{category.createdAt}</td>
-                                <td>
-                                    <button className="btnUpdateTable" type = "submit" onClick = {() => handleClickUpdate(category.id)}>Editar</button>
-                                    <button className="btnDeleteTable" type = "submit" onClick = {() => handleClickDelete(category.id)}>Eliminar</button>
-                                </td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
+            <div className='table-container-responsive'>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <td>
+                                <Link to="/backoffice/categories/create" className="btnAddTable">Crear</Link>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Creado</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {categories.map(category => {
+                            return (
+                                <tr key={category.id}>
+                                    <td>{category.name}</td>
+                                    <td>{category.createdAt}</td>
+                                    <td>
+                                        <button className="btnUpdateTable" type="button" onClick={() => handleClickUpdate(category.id)}>Editar</button>
+                                        <button className="btnDeleteTable" type="button" onClick={() => handleClickDelete(category.id)}>Eliminar</button>
+                                    </td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            </div>
         </section>
     )
 }
