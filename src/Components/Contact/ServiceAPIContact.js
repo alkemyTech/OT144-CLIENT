d5d75f85bc0f5506322a1f56ecbeea2b2ce265bf
Braifz <braian94lp@gmail.com>
2022-03-01 19:01:29 -0300

    add async/await

--- src/Components/Contact/ServiceAPIContact.js
+++ src/Components/Contact/ServiceAPIContact.js
@@ -5,8 +5,9 @@ import {
   putRequest,
 } from "../../Services/privateApiService";
 
-export const getContact = () => getRequest("contacts");
-export const getContactId = (id) => getRequest("contacts", id);
-export const postContact = (body) => postRequest("contacts", body);
-export const deleteContact = (id) => deleteRequest("contacts", id);
-export const updateContact = (id, body) => putRequest("contacts", id, body);
+export const getContact = async () => await getRequest("contacts");
+export const getContactId = async (id) => await getRequest("contacts", id);
+export const postContact = async (body) => await postRequest("contacts", body);
+export const deleteContact = async (id) => await deleteRequest("contacts", id);
+export const updateContact = async (id, body) =>
+  await putRequest("contacts", id, body);
