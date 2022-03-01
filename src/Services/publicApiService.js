aca57be186859375b767319f4a5f31f05e34fc44
Braifz <braian94lp@gmail.com>
2022-03-01 19:11:43 -0300

    change exports and strings

--- src/Services/publicApiService.js
+++ src/Services/publicApiService.js
@@ -3,11 +3,11 @@ import { baseURL } from "./Api";
 
 const config = {
   headers: {
-    Group: 01, //Aqui va el ID del equipo!!
+    Group: 144,
   },
 };
 
-const Get = () => {
+export const Get = () => {
   axios
     .get("https://jsonplaceholder.typicode.com/users", config)
     .then((res) => console.log(res))
@@ -16,11 +16,9 @@ const Get = () => {
 
 export const postRequest = async (url, body) => {
   try {
-    const response = await axios.post(baseURL + url, body);
+    const response = await axios.post(`${baseURL}${url}`, body);
     return response;
   } catch (e) {
     alert("Error al enviar la información!");
   }
 };
-
-export default Get;
