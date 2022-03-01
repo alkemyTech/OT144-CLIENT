e8afb305896f81aa8f79ad5de01c7a079dd08e08
galo <67323030+gfalotico@users.noreply.github.com>
2022-03-01 17:45:09 -0300

    requests

--- src/Services/userService.js
+++ src/Services/userService.js
@@ -1,1 +1,52 @@
 import {getRequest,deleteRequest,postRequest,putRequest} from './privateApiService'
+const endpoint = '/users'
+
+export const getUsers = async () => {
+    try{
+        const response = await getRequest(endpoint)
+        return response.data
+    }
+    catch(error){
+        throw error
+    }
+}
+    
+export const getUser = async (id) => {
+    try{
+        const response = await getRequest(endpoint, id)
+        return response.data
+    }
+    catch(error){
+        throw error
+    }
+}
+
+export const createUser = async (user) => {
+    try{
+        const response = await postRequest(endpoint,user)
+        return response.data
+    }
+    catch(error){
+        throw error
+    }
+}
+
+export const updateUser = async (id,user) => {
+    try{
+        const response = await putRequest(endpoint,id,user)
+        return response.data
+    }
+    catch(error){
+        throw error
+    }
+}
+
+export const deleteUser = async (id) => {
+    try{
+        const response = await deleteRequest(endpoint,id)
+        return response.data
+    }
+    catch(error){
+        throw error
+    }
+}
