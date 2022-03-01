7af63cf42fde520bdf0694b0dff02e0060cab61e
Braifz <braian94lp@gmail.com>
2022-03-01 00:30:31 -0300

    create method postRequest

--- src/Services/publicApiService.js
+++ src/Services/publicApiService.js
@@ -1,15 +1,26 @@
-import axios from 'axios';
+import axios from "axios";
+import { baseURL } from "./Api";
 
 const config = {
-    headers: {
-        Group: 01                //Aqui va el ID del equipo!!
-    }
-}
+  headers: {
+    Group: 01, //Aqui va el ID del equipo!!
+  },
+};
 
 const Get = () => {
-    axios.get('https://jsonplaceholder.typicode.com/users', config)
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-}
+  axios
+    .get("https://jsonplaceholder.typicode.com/users", config)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+};
 
-export default Get
+export const postRequest = async (url, body) => {
+  try {
+    const response = await axios.post(baseURL + url, body);
+    return response;
+  } catch (e) {
+    alert("Error al enviar la información!");
+  }
+};
+
+export default Get;
