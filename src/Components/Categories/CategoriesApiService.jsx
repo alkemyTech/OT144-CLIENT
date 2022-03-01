cdafc2b5b11b01f785670e487779988123cdf564
juliandebra <julian.debra25@gmail.com>
2022-02-28 21:30:38 -0300

    Categories HTTP extensions for endpoints

---
+++ src/Components/Categories/CategoriesApiService.jsx
@@ -0,0 +1,33 @@
+import {getRequest, postRequest, putRequest, deleteRequest} from './publicApiService';
+
+export const getCategories = async (search, skip, limit) => {
+    const endPoint = `/categories?search=${search}&skip=${skip}&limit=${limit}`;
+    const response = await getRequest(endPoint);
+    return response;
+};
+
+export const postCategories = async (data) => {
+    const endPoint = '/categories';
+    const response = await postRequest(endPoint, data);
+    return response;
+}
+
+export const getCategory = async (id) => {
+    const endPoint = `/category/${id}`;
+    const response = await getRequest(endPoint);
+    return response;
+}
+
+export const putCategory = async (id, data) => {
+    const endPoint = `/category/${id}`;
+    const response = await putRequest(endPoint, data);
+    return response;
+}
+
+export const deleteCategory = async (id) => {
+    const endPoint = `/category/${id}`;
+    const response = await deleteRequest(endPoint);
+    return response;
+}
+
+
