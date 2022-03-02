9fecab3a4130c3cc71dbf1353b0e02269a05047b
Marlon Gómez <marlonjulian15@gmail.com>
2022-03-02 10:18:43 -0500

    Delete consts

--- src/Services/slidesService.js
+++ src/Services/slidesService.js
@@ -5,32 +5,27 @@ import {
   deleteRequest,
 } from "./publicApiService";
 
-export const getSlides = async (search = "", skip = "", limit = "") => {
-  const endPoint = `/slides?search=${search}&skip=${skip}&limit=${limit}`;
-  const response = await getRequest(endPoint);
+export const getSlides = async (url) => {
+  const response = await getRequest(url);
   return response;
 };
 
 export const getSlide = async (id) => {
-  const endPoint = `/slides`;
-  const response = await getRequest(endPoint, id);
+  const response = await getRequest('/slides', id);
   return response;
 };
 
 export const createSlide = async (data) => {
-  const endPoint = `/slides`;
-  const response = await postRequest(endPoint, data);
+  const response = await postRequest('/slides', data);
   return response;
 };
 
 export const updateSlide = async (data) => {
-  const endPoint = `/slides`;
-  const response = await putRequest(endPoint, data.id, data);
+  const response = await putRequest('/slides', data.id, data);
   return response;
 };
 
 export const deleteSlide = async (id) => {
-  const endPoint = `/slides`;
-  const response = await deleteRequest(endPoint, id);
+  const response = await deleteRequest('/slides', id);
   return response;
 };
