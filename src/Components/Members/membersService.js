9ec32fd7a93b8636688992b1cddf28328069eaf8
cesar londoño <slac_@hotmail.com>
2022-03-01 21:33:53 -0300

    ok members

---
+++ src/Components/Members/membersService.js
@@ -0,0 +1,32 @@
+import {getRequest, postRequest, deleteRequest, putRequest} from "../../Services/privateApiService"
+
+export const getAllMembers = async (search = "", skip = "", limit = "") => {
+    const endPoint = `/members?search=${search}&skip=${skip}&limit=${limit}`
+    const response =  await getRequest(endPoint)
+    return response
+}
+
+export const getMember = async (id) => {
+    const endPoint = `/members/${id}`
+    const response = await getRequest(endPoint)
+    return response 
+}
+
+export const createMember = async (data) => {
+    const endPoint = `/members`
+    const response = await postRequest(endPoint, data)
+    return response 
+}
+
+export const updateMember = async (id) => {
+    const endPoint = `/members/${id}`
+    const response = await putRequest(endPoint)
+    return response 
+}
+
+export const deleteMember = async (id) => {
+    const endPoint = `/members/${id}`
+    const response = await deleteRequest(endPoint)
+    return response 
+}
+
