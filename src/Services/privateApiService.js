43e05c314349183934758a427fd22dad07b34f29
galo <67323030+gfalotico@users.noreply.github.com>
2022-03-02 01:11:23 -0300

    feat: private post method
    Co-authored-by: Federico Miranda <federico.mirandaa@gmail.com>

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,46 +1,72 @@
-import axios from 'axios';
-import { baseURL } from './Api';
+import axios from "axios";
+import { baseURL } from "./Api";
 
 const getToken = () => {
-    const token = localStorage.getItem("token") === "undefined"
-    ? ""
-    : localStorage.getItem("token")
-    || "";
-    return {
-        Authorization: `Bearer ${token}`
-    };
+  const token =
+    localStorage.getItem("token") === "undefined"
+      ? ""
+      : localStorage.getItem("token") || "";
+  return {
+    Authorization: `Bearer ${token}`,
+  };
 };
 
 const config = {
-    headers: {
-        Group: 144,
-    }
-}
+  headers: {
+    Group: 144,
+  },
+};
 
-export const getRequest =  async (url, id = null) => {
-    try {
-        const response = await axios({
-            method: 'get',
-            url: id === null ? `${baseURL}${url}` : `${baseURL}${url}/${id}`,
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': getToken(),
-            }
-        })
-        
-        return {
-            status: response.status,
-            data: response.data
-        }
-    }
-    catch (error) {
-        return {
-            status: error.response.status,
-            error: error.message,
-            data: error.response.data
-        }
-    }
-}
+export const postRequest = async (url, bodyData) => {
+  try {
+    let response = await axios({
+      method: "post",
+      url: `${baseURL}/${url}`,
+      data: bodyData,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: getToken(),
+        ...config.headers,
+      },
+    });
+
+    return {
+      status: response.status,
+      data: response.data,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      status: error.response.status,
+      error: error,
+      data: error.response.data,
+    };
+  }
+};
+
+export const getRequest = async (url, id = null) => {
+  try {
+    const response = await axios({
+      method: "get",
+      url: id === null ? `${baseURL}${url}` : `${baseURL}${url}/${id}`,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: getToken(),
+      },
+    });
+
+    return {
+      status: response.status,
+      data: response.data,
+    };
+  } catch (error) {
+    return {
+      status: error.response.status,
+      error: error.message,
+      data: error.response.data,
+    };
+  }
+};
 
 export const patchRequest = async(enpoint, id, data) => {
     try {
@@ -93,26 +119,25 @@ export const deleteRequest = async (url, id) => {
 }
 
 export const putRequest = async (endpoint, id, body) => {
-    try {
-        const response = await axios({
-            method: 'put',
-            url:`${baseURL}${endpoint}/${id}`,
-            data: body,
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': getToken(),
-            }
-        })
-        return {
-            status: response.status,
-            data: response.data
-        }
-    }
-    catch (error) {
-        return {
-            status: error.response.status,
-            error: error.message,
-            data: error.response.data
-        }
-    }
-}
+  try {
+    const response = await axios({
+      method: "put",
+      url: `${baseURL}${endpoint}/${id}`,
+      data: body,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: getToken(),
+      },
+    });
+    return {
+      status: response.status,
+      data: response.data,
+    };
+  } catch (error) {
+    return {
+      status: error.response.status,
+      error: error.message,
+      data: error.response.data,
+    };
+  }
+};
