e7441b790e807ed7862a2b125e5827eb0033f0aa
Marlon Gómez <49167174+mgomez08@users.noreply.github.com>
2022-03-02 17:21:37 -0500

    feat: http methods for slides
    * Create Slides service
    
    * Change name of variables
    
    * Add async method and return response
    
    * Rename file
    
    * Change parameters
    
    * Delete consts

---
+++ src/Services/slidesService.js
@@ -0,0 +1,31 @@
+import {
+  getRequest,
+  postRequest,
+  putRequest,
+  deleteRequest,
+} from "./publicApiService";
+
+export const getSlides = async (url) => {
+  const response = await getRequest(url);
+  return response;
+};
+
+export const getSlide = async (id) => {
+  const response = await getRequest('/slides', id);
+  return response;
+};
+
+export const createSlide = async (data) => {
+  const response = await postRequest('/slides', data);
+  return response;
+};
+
+export const updateSlide = async (data) => {
+  const response = await putRequest('/slides', data.id, data);
+  return response;
+};
+
+export const deleteSlide = async (id) => {
+  const response = await deleteRequest('/slides', id);
+  return response;
+};
