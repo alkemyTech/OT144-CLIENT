bb9c8d2905309460187dc3553370cf95c9c36257
Marlon Gómez <marlonjulian15@gmail.com>
2022-03-04 09:10:31 -0500

    Fix use of actions

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,46 +1,42 @@
 import axios from "axios";
 import { baseURL } from "../Services/Api";
-import types from "../types";
+import { login, logout, register } from "./actions";
 
-export const login = (email, password) => async (dispatch) => {
+export const startLogin = (email, password) => async (dispatch) => {
   const response = await axios.post(`${baseURL}login`, { email, password });
   if (response.status === 200) {
-    dispatch({
-      type: types.authLogin,
-      payload: {
+    dispatch(
+      login({
         token: response.data.data.token,
         user: response.data.data.user,
-      },
-    });
+      })
+    );
     localStorage.setItem("token", response.data.data.token);
   } else {
     //TODO: Alert login failed
   }
 };
 
-export const register = (name, email, password) => async (dispatch) => {
+export const startRegister = (name, email, password) => async (dispatch) => {
   const response = await axios.post(`${baseURL}register`, {
     name,
     email,
     password,
   });
   if (response.status === 200) {
-    dispatch({
-      type: types.authLogin,
-      payload: {
+    dispatch(
+      register({
         token: response.data.data.token,
         user: response.data.data.user,
-      },
-    });
+      })
+    );
     localStorage.setItem("token", response.data.data.token);
   } else {
     //TODO: Alert register failed
   }
 };
 
-export const logout = () => (dispatch) => {
+export const startLogout = () => (dispatch) => {
   localStorage.removeItem("token");
-  dispatch({
-    type: types.authLogout,
-  });
+  dispatch(logout());
 };
