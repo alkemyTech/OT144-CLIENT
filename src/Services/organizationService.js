c53c0a168150737e4e1fc7952393bef900547269
Marlon Gómez <marlonjulian15@gmail.com>
2022-03-04 08:46:47 -0500

    Create organization service

---
+++ src/Services/organizationService.js
@@ -0,0 +1,23 @@
+import { getRequest, postRequest, putRequest } from "./publicApiService";
+
+const endpoint = process.env.REACT_APP_URL_ORGANIZATION;
+
+export const getAllOrganizationData = async (url) => {
+  const response = await getRequest(endpoint);
+  return response;
+};
+
+export const getOrganizationData = async (id) => {
+  const response = await getRequest(endpoint, id);
+  return response;
+};
+
+export const createOrganizationData = async (data) => {
+  const response = await postRequest(endpoint, data);
+  return response;
+};
+
+export const updateOrganizationData = async (data) => {
+  const response = await putRequest(endpoint, data.id, data);
+  return response;
+};
