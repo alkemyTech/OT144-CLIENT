011f15988f7b632c64c76d97e4f7adc7a25750c0
cesar londoño <slac_@hotmail.com>
2022-03-04 21:30:46 -0300

    firts form slides

--- src/Components/Slides/SlidesForm.js
+++ src/Components/Slides/SlidesForm.js
@@ -1,38 +1,150 @@
 import React, { useState } from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import Actividades from '../Activities/Actividades';
 import '../FormStyles.css';
 import TitleComponent from '../title/TitleComponent';
 
 const SlidesForm = () => {
-    const [initialValues, setInitialValues] = useState({
-        name: '',
-        description: ''
-    });
-
-    const handleChange = (e) => {
-        if(e.target.name === 'name'){
-            setInitialValues({...initialValues, name: e.target.value})
-        } if(e.target.name === 'description'){
-            setInitialValues({...initialValues, description: e.target.value})
+    const SlidesForm = () => {
+    
+        const validation = (values) => {
+            let errores = {}
+    
+                //Validacion Name
+                if(!values.name){
+                    errores.name = 'Por favor ingresa un nombre'
+                }else if(!/^[a-zA-ZÀ-ÿ\s]{4,40}$/.test(values.name)){
+                    errores.name = 'El nombre solo puede contener letras y espacios'
+                }
+                //Validacion Order
+                if(!values.order){
+                    errores.order = 'Por favor ingrese un numero de orden'
+                }
+                //Validacion Imagen
+                if(!values.img){
+                    errores.img = 'Por favor ingrese una imagen'
+                }else if(!(/\.(jpg|png)$/i).test(values.img)) {
+                    errores.img = 'El archivo a adjuntar no es una imagen'
+                }
+                //Validacion Order
+                if(!values.description){
+                    errores.description = 'Por favor ingrese una descripción'
+                }
+                return errores;
         }
+    
+        const handleSubmit = (values) => {
+            console.log('Desde Handle', values)
+        }
+    
+        return (
+    
+            <Formik
+                initialValues={{
+                    name:'',
+                    order:'',
+                    img:'',
+                    description:''
+                    
+                }}
+                validate={validation}
+                onSubmit={(values, {resetForm}) => {
+                    resetForm();
+                    handleSubmit(values)
+                } }
+                
+            >
+                {({errors}) => {
+                    // console.log(errors)
+                    return (
+    
+                        <Form className="formulario" >
+                            
+                            <label>Slide Admin</label>
+                                <div className='campo'>
+                                    
+                                    <Field
+                                        className="input-field"
+                                        id="name"
+                                        name="name"
+                                        type="text"
+                                        placeholder="Nombre"
+                                    />
+                                    
+                                    <ErrorMessage name="name" component={() => (
+                                        <div>
+                                            { errors.name}
+                                        </div>
+                                    )} />
+                                       
+                                    <Field
+                                        className="input-field"
+                                        id="order"
+                                        name= "order"
+                                        type="text"
+                                        placeholder="Order"
+                                    />
+                                    <ErrorMessage name="order" component={() => (
+                                        <div>
+                                            { errors.order}
+                                        </div>
+                                    )} />
+                                    
+                                    <Field
+                                    
+                                        id="img"
+                                        type="file"
+                                        name="img"
+                                    />
+                                    {errors.img &&
+                                        <div>
+                                            { errors.img}
+                                        </div>
+                                    }
+    
+                                    <CKEditor
+                                        editor={ ClassicEditor }
+                                        data=""
+                                        onReady={ editor => {
+                                            // You can store the "editor" and use when it is needed.
+                                            console.log( 'Editor is ready to use!', editor );
+                                        } }
+                                        onChange={ ( event, editor ) => {
+                                            const data = editor.getData();
+                                            console.log( { event, editor, data } );
+                                        } }
+                                        onBlur={ ( event, editor ) => {
+                                            console.log( 'Blur.', editor );
+                                        } }
+                                        onFocus={ ( event, editor ) => { 
+                                            console.log( 'Focus.', editor );
+                                        } }
+                                    />
+                                    
+                                    <Field
+                                        as="textarea"
+                                        className="input-img"
+                                        id="description"
+                                        name="description"
+                                        placeholder="Descripción"
+                                    />
+                                    {errors.description &&
+                                        <div>
+                                            { errors.description}
+                                        </div>
+                                    }
+                                </div>
+                                <Field  className="submit-btn" type="submit" name="enviar" value="Enviar"/>
+                                <button type="submit" className="submit-btn">enviar</button>
+                        </Form>
+                        )
+                } }
+            </Formik>
+    
+        );
     }
-
-    const handleSubmit = (e) =>{
-        e.preventDefault();
-        console.log(initialValues);
-    }
-
-    return (
-        <>
-        
-        <form className="form-container" onSubmit={handleSubmit}>
-            <input className="input-field" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Slide Title"></input>
-            <input className="input-field" type="text" name="description" value={initialValues.description} onChange={handleChange} placeholder="Write the description"></input>
-            <button className="submit-btn" type="submit">Send</button>
-        </form>
-        </>
-        
-    );
 }
  
 export default SlidesForm;
