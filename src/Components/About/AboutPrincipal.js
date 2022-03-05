cb705749b14091630e19c1ee36b984407d0a2ee6
Braifz <braian94lp@gmail.com>
2022-03-05 17:49:52 -0300

    add loading and error without Spinner,Alert

--- src/Components/About/AboutPrincipal.js
+++ src/Components/About/AboutPrincipal.js
@@ -1,11 +1,60 @@
+import { useEffect, useState } from "react";
 import AboutUs from "./AboutUs";
 
-export default function AboutPrincipal(){
-    const principalText= "Desde 1997 en Somos Más trabajamos con los chicos y chicas, mamás y papás, abuelos y vecinos del barrio La Cava generando procesos de crecimiento y de inserción social. Uniendo las manos de todas las familias, las que viven en el barrio y las que viven fuera de él, es que podemos pensar, crear y garantizar estos procesos. ﻿ Somos una asociación civil sin fines de lucro que se creó en 1997 con la intención de dar alimento a las familias del barrio. Con el tiempo fuimos involucrándonos con la comunidad y agrandando y mejorando nuestra capacidad de trabajo. Hoy somos un centro comunitario que acompaña a más de 700 personas a través de las áreas de: Educación, deportes, primera infancia, salud, alimentación y trabajo social."
+export default function AboutPrincipal() {
+  const [state, setState] = useState({
+    loading: false,
+    data: [],
+    error: null,
+  });
 
-    return(
-        <main className="aboutPrincipal-container">
-            <AboutUs text={principalText}/>
-        </main>
-    )
+  const { loading, data, error } = state;
+
+  const principalText =
+    "Desde 1997 en Somos Más trabajamos con los chicos y chicas, mamás y papás, abuelos y vecinos del barrio La Cava generando procesos de crecimiento y de inserción social. Uniendo las manos de todas las familias, las que viven en el barrio y las que viven fuera de él, es que podemos pensar, crear y garantizar estos procesos. ﻿ Somos una asociación civil sin fines de lucro que se creó en 1997 con la intención de dar alimento a las familias del barrio. Con el tiempo fuimos involucrándonos con la comunidad y agrandando y mejorando nuestra capacidad de trabajo. Hoy somos un centro comunitario que acompaña a más de 700 personas a través de las áreas de: Educación, deportes, primera infancia, salud, alimentación y trabajo social.";
+
+  useEffect(() => {
+    const doFetchData = async () => {
+      setState({
+        loading: true,
+        data: [],
+        error: null,
+      });
+      try {
+        const data = await fetch("x"); // Acá va el GET a la API
+        setState({
+          loading: false,
+          data: data,
+          error: null,
+        });
+      } catch (e) {
+        setState({
+          loading: false,
+          data: [],
+          error: e.message,
+        });
+      }
+    };
+    doFetchData();
+  }, []);
+
+  if (loading === true) {
+    return <p>loading...</p>; // Acá va el spinner!
+  }
+
+  if (error) {
+    return (
+      // Acá va el Alert Error!
+      <div>
+        <p>Error!!!</p>
+        <p>{error.message}</p>
+      </div>
+    );
+  } else {
+    return (
+      <main className="aboutPrincipal-container">
+        <AboutUs text={principalText} />
+      </main>
+    );
+  }
 }
--- src/Components/Home/HomePage.js
+++ src/Components/Home/HomePage.js
@@ -1,18 +1,64 @@
-import React from "react";
-import "./stylesHomePage.css"
+import React, { useEffect, useState } from "react";
+import "./stylesHomePage.css";
 
-function HomePage(){
-    return(
-        <main className="homePage">
-            <section className="containerSlider">
-                <h2>Aqui va el componente Slider</h2>
-            </section>
+function HomePage() {
+  const [state, setState] = useState({
+    loading: false,
+    data: [],
+    error: null,
+  });
 
-            <section className="containerNovedades">
-                <h2>Aqui va el componente Novedades</h2>
-            </section>
-        </main>
-    )
+  const { loading, data, error } = state;
+
+  useEffect(() => {
+    const doFetchData = async () => {
+      setState({
+        loading: true,
+        data: [],
+        error: null,
+      });
+      try {
+        const data = await fetch("x"); // Acá va el GET a la API
+        setState({
+          loading: false,
+          data: data,
+          error: null,
+        });
+      } catch (e) {
+        setState({
+          loading: false,
+          data: [],
+          error: e.message,
+        });
+      }
+    };
+    doFetchData();
+  }, []);
+
+  if (loading === true) {
+    return <p>loading...</p>; // Acá va el spinner!
+  }
+
+  if (error) {
+    return (
+      // Acá va el Alert Error!
+      <div>
+        <p>Error!!!</p>
+        <p>{error.message}</p>
+      </div>
+    );
+  }
+  return (
+    <main className="homePage">
+      <section className="containerSlider">
+        <h2>Aqui va el componente Slider</h2>
+      </section>
+
+      <section className="containerNovedades">
+        <h2>Aqui va el componente Novedades</h2>
+      </section>
+    </main>
+  );
 }
 
-export default HomePage
+export default HomePage;
