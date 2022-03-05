f5aa101d8e8484d136c52c39dfe16328ab14146b
marsl27 <marielceciliaromeo@hotmail.com>
2022-03-05 13:15:54 -0300

    [ADD] add class atribute to lazyLoad

--- src/Components/UI/LazyLoadImages/LazyLoadImages.jsx
+++ src/Components/UI/LazyLoadImages/LazyLoadImages.jsx
@@ -1,11 +1,12 @@
 import LazyLoad from 'react-lazyload';
 
-export default function LazyLoadImages({ src, altText }) {
+export default function LazyLoadImages({ src, altText, classText }) {
   return (
     <LazyLoad>
       <img
         src={src}
         alt={altText}
+        className={classText}
       />
     </LazyLoad>
   )
