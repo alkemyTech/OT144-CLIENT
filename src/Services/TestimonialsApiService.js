ad499011e31cedaa0adb8eb6267872f4af5bfb6b
juliandebra <julian.debra25@gmail.com>
2022-03-05 00:43:36 -0300

    CC corrected

--- src/Services/TestimonialsApiService.js
+++ src/Services/TestimonialsApiService.js
@@ -1,27 +1,27 @@
 import {getRequest, postRequest, putRequest, deleteRequest} from '../../Services/publicApiService';
-const endPoint = `${process.env.REACT_APP_API_TESTIMONIALS}`;
+const endpoint = process.env.REACT_APP_API_TESTIMONIALS;
 
 export const getTestimonials = async () => {
-    const response = await getRequest(endPoint);
+    const response = await getRequest(endpoint);
     return response;
 };
 
 export const createTestimonials = async (data) => {
-    const response = await postRequest(endPoint, data);
+    const response = await postRequest(endpoint, data);
     return response;
 }
 
 export const getTestimonial = async (id) => {
-    const response = await getRequest(endPoint, id);
+    const response = await getRequest(endpoint, id);
     return response;
 }
 
 export const updateTestimonial = async (id, data) => {
-    const response = await putRequest(endPoint, id, data);
+    const response = await putRequest(endpoint, id, data);
     return response;
 }
 
 export const deleteTestimonial = async (id) => {
-    const response = await deleteRequest(endPoint, id);
+    const response = await deleteRequest(endpoint, id);
     return response;
 }
