348f881027c33dabbed79a4710a6d4a357092d8f
galo <67323030+gfalotico@users.noreply.github.com>
2022-03-06 17:36:49 -0300

    fixes in types

--- src/types/index.js
+++ src/types/index.js
@@ -2,8 +2,8 @@ export const AUTH_LOGIN = "[AUTH]Login";
 export const AUTH_REGISTER = "[AUTH]Register";
 export const AUTH_LOGOUT = "[AUTH]Logout";
 
-export const CATEGORIES_GET = "[CATEGORIES_GET]Get";
-export const CATEGORIES_SET = "[CATEGORIES_GET]Get";
-export const CATEGORIES_ADD = "[CATEGORIES_GET]Add";
-export const CATEGORIES_DELETE = "[CATEGORIES_GET]Delete";
-export const CATEGORIES_UPDATE = "[CATEGORIES_GET]Update";
+export const CATEGORIES_GET = "[CATEGORIES]Get";
+export const CATEGORIES_SET = "[CATEGORIES]set";
+export const CATEGORIES_ADD = "[CATEGORIES]Add";
+export const CATEGORIES_DELETE = "[CATEGORIES]Delete";
+export const CATEGORIES_UPDATE = "[CATEGORIES]Update";
