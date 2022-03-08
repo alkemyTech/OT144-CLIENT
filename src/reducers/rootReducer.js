84934e2a1e347dfb67b8a7a74f0b8dafb5bf434a
galo <67323030+gfalotico@users.noreply.github.com>
2022-03-07 22:47:07 -0300

    feat: categories reducer
    * base
    
    * fixes in types
    
    * fixes
    
    * Console.log()
    * endpoint
    
    * fix
    
    * Reducer categories
    
    Co-authored-by: Federico Miranda <federico.mirandaa@gmail.com>

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import newsReducer from "./newsReducer";
+import categoriesReducer from "./categories";
 
 export const rootReducer = combineReducers({
   auth: authReducer,
-  news: newsReducer
+  categories: categoriesReducer,
+  news: newsReducer,
 });
