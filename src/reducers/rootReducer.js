2dbaa80df0dd05d3ccaf26fbdb0f4a41f6b67ad3
Braifz <braian94lp@gmail.com>
2022-03-08 15:03:11 -0300

    add memberReducer to rootReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
+import { memberReducer } from "./memberReducer";
 
 export const rootReducer = combineReducers({
   auth: authReducer,
+  members: memberReducer,
 });
