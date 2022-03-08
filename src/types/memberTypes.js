2ecf8611c9f30957f6557a2fda0171ce203147da
Braifz <braian94lp@gmail.com>
2022-03-08 14:56:19 -0300

    add types

---
+++ src/types/memberTypes.js
@@ -0,0 +1,5 @@
+export const MEMBER_GET = "[MEMBER_GET]Get";
+export const MEMBER_SET = "[MEMBER_SET]Get";
+export const MEMBER_ADD = "[MEMBER_ADD]add";
+export const MEMBER_UPDATE = "[MEMBER_UPDATE]update";
+export const MEMBER_DELETE = "[MEMBER_DELETE]delete";
