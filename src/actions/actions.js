1e15409f690ac2cf683a749dd96e91a07cc20b89
Mariel Romeo <marielceciliaromeo@hotmail.com>
2022-03-07 22:20:38 -0300

    feat: news reducer
    * [ADD] newsReducer with types and actions. Conection news component with http service
    
    * [DELETE] console.log
    
    * [FIX] suggested changes
    
    Co-authored-by: Federico Miranda <federico.mirandaa@gmail.com>

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -21,3 +21,11 @@ export const register = (user) => {
 };
 
 export const logout = () => ({ type: types.AUTH_LOGOUT });
+
+export const setNewsAction = (news) => ({ type: types.NEWS_GET, payload: news });
+
+export const addNewsAction = (news) => ({ type: types.NEWS_ADD, payload: news });
+
+export const updateNewsAction = (news) => ({ type: types.NEWS_UPDATE, payload: news });
+
+export const deleteNewsAction = (id) => ({ type: types.NEWS_DELETE, payload: id });
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
+import newsReducer from "./newsReducer";
 
 export const rootReducer = combineReducers({
   auth: authReducer,
+  news: newsReducer
 });
