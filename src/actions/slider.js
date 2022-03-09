f23dd1fcd1e1d97b35251fa3308e1955908df79f
Braifz <braian94lp@gmail.com>
2022-03-09 00:10:50 -0300

    create slider actions

---
+++ src/actions/slider.js
@@ -0,0 +1,26 @@
+import {
+  SLIDER_GET,
+  SLIDER_ADD,
+  SLIDER_DELETE,
+  SLIDER_UPDATE,
+} from "../types/index";
+
+export const getSliderActions = (slider) => ({
+  type: SLIDER_GET,
+  payload: slider,
+});
+
+export const addSliderActions = (slider) => ({
+  type: SLIDER_ADD,
+  payload: slider,
+});
+
+export const updateSliderActions = (slider) => ({
+  type: SLIDER_UPDATE,
+  payload: slider,
+});
+
+export const deleteSliderActions = (id) => ({
+  type: SLIDER_DELETE,
+  payload: id,
+});
