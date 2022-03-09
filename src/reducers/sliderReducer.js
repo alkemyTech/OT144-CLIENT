fc666c1fe021e3cb39ae4dd14ddbb8abb4c4c29e
Braifz <braian94lp@gmail.com>
2022-03-09 00:30:47 -0300

    create sliderReducer.js

---
+++ src/reducers/sliderReducer.js
@@ -0,0 +1,36 @@
+import {
+  SLIDER_GET,
+  SLIDER_ADD,
+  SLIDER_UPDATE,
+  SLIDER_DELETE,
+} from "../types/index";
+
+const initialState = {
+  sliders: [],
+};
+
+export const sliderReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SLIDER_GET:
+      return {
+        sliders: action.payload,
+      };
+    case SLIDER_ADD:
+      return {
+        sliders: [...state, action.payload],
+      };
+    case SLIDER_UPDATE:
+      return {
+        sliders: state.sliders.map(() =>
+          el.id === action.payload.id ? action.payload : el
+        ),
+      };
+    case SLIDER_DELETE:
+      return {
+        sliders: state.sliders.filter((el) => el.id !== action.payload),
+        ...state,
+      };
+    default:
+      return state;
+  }
+};
