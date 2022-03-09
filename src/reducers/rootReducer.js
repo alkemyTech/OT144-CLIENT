5c6237485204cd4c9a52b2e1f0b8b4cc54c517c4
Braifz <braian94lp@gmail.com>
2022-03-09 00:38:36 -0300

    add sliderReducer to rootReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import newsReducer from "./newsReducer";
 import categoriesReducer from "./categories";
+import sliderReducer from "./sliderReducer";
 
 export const rootReducer = combineReducers({
   auth: authReducer,
   categories: categoriesReducer,
   news: newsReducer,
+  sliders: sliderReducer,
 });
