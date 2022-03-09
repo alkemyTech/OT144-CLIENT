6e9108998f3f266e1377577f8d6f2f77d1a66bfa
Braifz <braian94lp@gmail.com>
2022-03-09 01:10:16 -0300

    change endpoint slidesService.js

--- src/Services/slidesService.js
+++ src/Services/slidesService.js
@@ -5,27 +5,29 @@ import {
   deleteRequest,
 } from "./publicApiService";
 
-export const getSlides = async (url) => {
-  const response = await getRequest(url);
+const endPoint = process.env.REACT_APP_URL_SLIDES;
+
+export const getSlides = async () => {
+  const response = await getRequest(endPoint);
   return response;
 };
 
 export const getSlide = async (id) => {
-  const response = await getRequest('/slides', id);
+  const response = await getRequest(endPoint, id);
   return response;
 };
 
 export const createSlide = async (data) => {
-  const response = await postRequest('/slides', data);
+  const response = await postRequest(endPoint, data);
   return response;
 };
 
 export const updateSlide = async (data) => {
-  const response = await putRequest('/slides', data.id, data);
+  const response = await putRequest(endPoint, data.id, data);
   return response;
 };
 
 export const deleteSlide = async (id) => {
-  const response = await deleteRequest('/slides', id);
+  const response = await deleteRequest(endPoint, id);
   return response;
 };
