af4216f2d9231bc900bc6bdc246d60756fbf757c
Braifz <braian94lp@gmail.com>
2022-03-09 02:05:36 -0300

    add loader, alert, and store data sliders reducer

--- src/Components/Slides/SlidesList.js
+++ src/Components/Slides/SlidesList.js
@@ -1,8 +1,34 @@
-import React from "react";
-import Table from "../UI/Table"
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import SpinnerComponent from "../UI/spinner/SpinnerComponent";
+import ErrorAlert from "../UI/Alerts/ErrorAlert";
+import Table from "../UI/Table";
+import { getSlides } from "../../Services/slidesService";
+import { store } from "../../app/store";
+import { getSliderActions } from "../../actions/slider";
 
 export default function SlidesList() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      try {
+        const response = await getSlides();
+        store.dispatch(getSliderActions(response.data.data));
+        setData(response.data.data);
+      } catch (e) {
+        return <ErrorAlert />;
+      }
+      setLoading(false);
+    })();
+  }, []);
+
+  if (loading) {
+    return <SpinnerComponent loading={true} />;
+  }
+
   return (
     <div>
       <Table
@@ -28,34 +54,13 @@ export default function SlidesList() {
             </td>
           ),
           actions: ({}) => (
-            <td >
+            <td>
               <button className="btnUpdateTable">Editar</button>
               <button className="btnDeleteTable">Eliminar</button>
             </td>
           ),
         }}
-        data={[
-          {
-            name: "Maria",
-            email: "gfalotioc123@gmail.com",
-            order: 13,
-            image: "https://picsum.photos/200/300",
-          },
-          {
-            name: "Maria",
-            email: "gfalotioc123@gmail.com",
-            order: 1,
-            image:
-              "https://images.unsplash.com/photo-1609591281156-3ca8c3836c5c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-          },
-          {
-            name: "Maria",
-            email: "gfalotioc123@gmail.com",
-            order: 23,
-            image:
-              "https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80",
-          },
-        ]}
+        data={data}
       />
     </div>
   );
