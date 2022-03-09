e73f8adec465b782b54ba6317ba1ec884b3bf45c
Braifz <braian94lp@gmail.com>
2022-03-08 21:05:44 -0300

    [fix typo]

--- src/Services/activitiesService.js
+++ src/Services/activitiesService.js
@@ -7,27 +7,31 @@ import {
 
 const endpoint = process.env.REACT_APP_API_ACTIVITIES;
 
-export const getNews = async () => {
-  const response = await getRequest(endpoint);
-  return response;
+export const getActivities = async () => {
+  try {
+    const response = await getRequest(endpoint);
+    return response;
+  } catch (e) {
+    alert;
+  }
 };
 
-export const getNewsById = async (id) => {
+export const getActivitiesId = async (id) => {
   const response = await getRequest(endpoint, id);
   return response;
 };
 
-export const postNews = async (bodyData) => {
+export const postActivities = async (bodyData) => {
   const response = await postRequest(endpoint, bodyData);
   return response;
 };
 
-export const deleteNews = async (id) => {
+export const deleteActivities = async (id) => {
   const response = await deleteRequest(endpoint, id);
   return response;
 };
 
-export const updateNews = async (id, bodyData) => {
+export const updateActivities = async (id, bodyData) => {
   const response = await putRequest(endpoint, id, bodyData);
   return response;
 };
