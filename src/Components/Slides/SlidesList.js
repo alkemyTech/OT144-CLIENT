cf7320dd72b6b81b1c6cfb15082ff99c17089112
Esteban Abella <75279487+EstebanAbella@users.noreply.github.com>
2022-03-08 23:22:20 -0300

    feat: env slides
    * Task/creacion variables de entorno e implementacion
    
    * Task/implementacion de los metodos http
    
    * Task/se modifico el ruteo para componente slides
    
    * Task/se agrego default en el archivo RedirecSlides
    
    Co-authored-by: unknown <esteban.abella1@gmail .com>
    Co-authored-by: Federico Miranda <federico.mirandaa@gmail.com>

--- src/Components/Slides/SlidesList.js
+++ src/Components/Slides/SlidesList.js
@@ -1,16 +1,61 @@
-import React from "react";
-import Table from "../UI/Table"
+import React, { useState, useEffect } from "react";
+import Table from "../UI/Table";
 import { Link } from "react-router-dom";
+import { getSlides, deleteSlide } from '../../Services/slidesService'
+
 
 export default function SlidesList() {
+
+  const [slidesData, setSlidesData] = useState([])
+  const [count, setCount] = useState(0)
+
+
+  useEffect(
+    ()=>{
+
+      const cast = async () => {
+        try{
+            const response = await Promise.resolve(getSlides())
+            return ( setSlidesData(response.data.data) )
+        }catch(error) {
+          return {
+            status: error.response.status,
+            error: error.message,
+            data: error.response.data,
+          }
+        }  
+      }
+
+      cast()
+    },
+    [count]
+  )
+
+  const handleClickDelete = async (id) => {
+    setCount(count + 1)
+    try{
+      const response = await Promise.resolve(deleteSlide(id))
+      return {
+        status: response.status,
+        data: response.data
+      }
+    }catch(error) {
+      return {
+        status: error.response.status,
+        error: error.message,
+        data: error.response.data,
+      }
+    }
+  }
+
   return (
     <div>
       <Table
         header={{
           title: "Listado Slides",
           button: () => (
-            <Link to="/backoffice/Slides/create">
-              <button className="btnAddTable">Crear Slide</button>
+            <Link to="/backoffice/slides/create">
+              <button className="btnAddTable" >Crear Slide</button>
             </Link>
           ),
         }}
@@ -27,35 +72,16 @@ export default function SlidesList() {
               <h1>{order}</h1>
             </td>
           ),
-          actions: ({}) => (
-            <td >
-              <button className="btnUpdateTable">Editar</button>
-              <button className="btnDeleteTable">Eliminar</button>
+          actions: ({ id }) => (
+            <td>
+              <Link to="/backoffice/slides/edit">
+                <button className="btnUpdateTable">Editar</button>
+              </Link>
+              <button className="btnDeleteTable" onClick={ () => handleClickDelete(id) }>Eliminar</button>
             </td>
           ),
         }}
-        data={[
-          {
-            name: "Maria",
-            email: "gfalotioc123@gmail.com",
-            order: 13,
-            image: "https://picsum.photos/200/300",
-          },
-          {
-            name: "Maria",
-            email: "gfalotioc123@gmail.com",
-            order: 1,
-            image:
-              "https://images.unsplash.com/photo-1609591281156-3ca8c3836c5c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-          },
-          {
-            name: "Maria",
-            email: "gfalotioc123@gmail.com",
-            order: 23,
-            image:
-              "https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80",
-          },
-        ]}
+        data={slidesData}
       />
     </div>
   );
