bb6bfb6c32c4fc89fb529c75d059c6695cf942ea
Marlon Gómez <marlonjulian15@gmail.com>
2022-03-11 11:07:06 -0500

    Add indent configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,21 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: [
-    'plugin:react/recommended',
-    'standard',
-    'prettier'
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
-    ecmaVersion: 12,
-    sourceType: 'module'
-  },
-  plugins: [
-    'react'
-  ],
-  "ignorePatterns": ["/public"],
-  rules: {
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off'
-  }
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ['plugin:react/recommended', 'standard', 'prettier'],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	plugins: ['react'],
+	ignorePatterns: ['/public'],
+	rules: {
+		'react/react-in-jsx-scope': 'off',
+		'react/prop-types': 'off',
+		indent: ['error', 'tab'],
+	},
 }
