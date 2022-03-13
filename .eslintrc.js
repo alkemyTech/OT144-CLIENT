2a7635f3e172675b3ae6285f9ce9a2e14505b529
Marlon Gómez <49167174+mgomez08@users.noreply.github.com>
2022-03-13 00:37:04 -0500

    feat: eslint + lint-staged + husky + prettier + sonarjs
    * Install eslint dependencies
    
    * Install dependecies for eslint with standard style
    
    * Create eslint config
    
    * Create prettier config files
    
    * Add ignore public folder
    
    * Add indent configuration
    
    * Add .eslintcache in .gitignore
    
    * Install husky, lint-staged and plugin-sonarjs dependencies
    
    * Add sonarjs plugin
    
    * Add .husky folder
    
    * Add setting to detect react version

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'plugin:react/recommended',
+		'standard',
+		'prettier',
+		'plugin:sonarjs/recommended',
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	plugins: ['react', 'sonarjs'],
+	ignorePatterns: ['/public'],
+	rules: {
+		'react/react-in-jsx-scope': 'off',
+		'react/prop-types': 'off',
+		indent: ['error', 'tab'],
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+}
