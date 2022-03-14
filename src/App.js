7c6bcd7711f782d7b5e4b237a82135ed39fe59f4
juliandebra <julian.debra25@gmail.com>
2022-03-14 18:47:01 -0300

    Task/Contact restriction for admin

--- src/App.js
+++ src/App.js
@@ -1,91 +1,82 @@
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import HomeForm from './Components/Home/HomeForm'
-import ActivitiesForm from './Components/Activities/ActivitiesForm/ActivitiesForm'
-import CategoriesForm from './Components/Categories/CategoriesForm'
-import DataScreen from './Components/Testimonials/DataScreen'
-import TestimonialForm from './Components/Testimonials/TestimonialsForm'
-import UserForm from './Components/Users/UsersForm'
-import SchoolCampaign from './Campaigns/School/SchoolCampaign'
-import ToysCampaign from './Campaigns/Toys/ToysCampaign'
-import MemberList from './Components/Members/MemberList'
-import MembersForm from './Components/Members/MembersForm'
-import ProjectsForm from './Components/Projects/ProjectsForm'
-import Activities from './Components/Activities/Actividades'
-import AboutPrincipal from './Components/About/AboutPrincipal'
-import LoginForm from './Components/Auth/LoginForm'
-import OrganizationEditForm from './Components/Home/BackOffice/OrganizationEditForm'
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomeForm from './Components/Home/HomeForm';
+import ActivitiesForm from './Components/Activities/ActivitiesForm/ActivitiesForm';
+import CategoriesForm from './Components/Categories/CategoriesForm';
+import DataScreen from './Components/Testimonials/DataScreen';
+import TestimonialForm from './Components/Testimonials/TestimonialsForm';
+import UserForm from './Components/Users/UsersForm';
+import SchoolCampaign from './Campaigns/School/SchoolCampaign';
+import ToysCampaign from './Campaigns/Toys/ToysCampaign';
+import MemberList from './Components/Members/MemberList';
+import MembersForm from './Components/Members/MembersForm';
+import ProjectsForm from './Components/Projects/ProjectsForm';
+import Activities from './Components/Activities/Actividades';
+import AboutPrincipal from './Components/About/AboutPrincipal';
+import LoginForm from './Components/Auth/LoginForm';
+import OrganizationEditForm from './Components/Home/BackOffice/OrganizationEditForm';
 import ScreenDashboardPage from './Components/ScreenDashboard/ScreenDashboardPage'
 import HomePage from './Components/Home/HomePage'
-import Register from './Components/Auth/RegisterForm'
-import Contact from './Components/Contact/Contact'
-import CreateNews from './Components/News/CreateNews'
-import NewsHome from './Components/News/Home'
-import UsersList from './Components/Users/UsersList/UsersList'
-import Novedades from './Components/Home/BackOffice/Novedades'
-import Donacion from './Components/Donations/Donacion'
-import Gracias from './Components/Donations/Gracias'
-import DetailNew from './Components/News/Detail/DetailNew'
-import ActivitiesDetail from './Components/Activities/Detail/ActivitiesDetail'
-import Categories from './Components/Home/BackOffice/Categories'
+import Register from './Components/Auth/RegisterForm';
+import Contact from './Components/Contact/Contact';
+import CreateNews from './Components/News/CreateNews';
+import NewsHome from './Components/News/Home';
+import UsersList from './Components/Users/UsersList/UsersList';
+import Novedades from './Components/Home/BackOffice/Novedades';
+import Donacion from './Components/Donations/Donacion';
+import Gracias from './Components/Donations/Gracias';
+import DetailNew from './Components/News/Detail/DetailNew';
+import ActivitiesDetail from './Components/Activities/Detail/ActivitiesDetail';
+import Categories from './Components/Home/BackOffice/Categories';
 import RedirecSlides from './Components/Slides/RedirecSlides'
-import SlidesList from './Components/Slides/SlidesList'
-import Newsletter from './Components/Newsletter/Newsletter'
-import UserNotLogged, { isLogin } from './Components/UI/Errors/UserNotLogged'
+import SlidesList from './Components/Slides/SlidesList';
+import Newsletter from './Components/Newsletter/Newsletter';
+import UserNotLogged from './Components/UI/Errors/UserNotLogged';
+import { contactRestrict } from './Components/Contact/ContactRestrict';
+import { isLogin } from './Components/UI/Errors/UserNotLogged';
 
 function App() {
-	return (
-		<>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" exact element={<HomePage />} />
-					<Route path="/contact" element={<Contact />} />
-					<Route path="/create-activity" element={<ActivitiesForm />} />
-					<Route path="/create-category" element={<CategoriesForm />} />
-					<Route path="/create-news" element={<CreateNews />} />
-					<Route
-						path="/backoffice/organization/edit-home"
-						element={<HomeForm />}
-					/>
-					<Route
-						path="/backoffice/slides/:action"
-						element={<RedirecSlides />}
-					/>
-					<Route path="/backoffice/slides" element={<SlidesList />} />
-					<Route
-						path="/backoffice/organization/edit"
-						element={<OrganizationEditForm />}
-					/>
-					<Route path="/backoffice/organization" element={<DataScreen />} />
-					<Route path="/backoffice/members" element={<MemberList />} />
-					<Route path="/backoffice/categories" element={<Categories />} />
-					<Route path="/backoffice/members/edit" element={<MembersForm />} />
-					<Route path="/backoffice/users" element={<UsersList />} />
-					<Route path="/create-testimonials" element={<TestimonialForm />} />
-					<Route path="/create-user" element={<UserForm />} />
-					<Route path="/create-member" element={<MembersForm />} />
-					<Route path="/create-project" element={<ProjectsForm />} />
-					<Route path="/school-campaign" element={<SchoolCampaign />} />
-					<Route path="/toys-campaign" element={<ToysCampaign />} />
-					<Route path="/about-us" element={<AboutPrincipal />} />
-					<Route path="/login" element={<LoginForm />} />
-					<Route path="/backoffice" element={<ScreenDashboardPage />} />
-					<Route path="/register" element={<Register />} />
-					<Route path="/backoffice/news" element={<Novedades />} />
-					<Route path="/donar" element={<Donacion />} />
-					<Route path="/gracias" element={<Gracias />} />
-					<Route path="/novedades/:id" element={<DetailNew />} />
-					<Route path="/activities" element={<Activities />} />
-					<Route path="/activities/:id" element={<ActivitiesDetail />} />
-					<Route path="/Novedades" element={<NewsHome />} />
-					<Route
-						path="/newsletter"
-						element={isLogin() ? <Newsletter /> : <UserNotLogged />}
-					/>
-				</Routes>
-			</BrowserRouter>
-		</>
-	)
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<HomePage/>} />
+          <Route path="/contact" element={ contactRestrict() ? <Contact/> : <HomePage/>} />
+          <Route path="/create-activity" element={<ActivitiesForm/>} />
+          <Route path="/create-category" element={<CategoriesForm/>} />
+          <Route path="/create-news" element={<CreateNews/>} />
+          <Route path="/backoffice/organization/edit-home" element={<HomeForm/>} />
+          <Route path="/backoffice/slides/:action" element={<RedirecSlides/>} />
+          <Route path="/backoffice/slides" element={<SlidesList/>} />
+          <Route path="/backoffice/organization/edit" element={<OrganizationEditForm/>} />
+          <Route path="/backoffice/organization" element={<DataScreen/>}/>
+          <Route path="/backoffice/members" element={<MemberList/>}/>
+          <Route path="/backoffice/categories" element={<Categories/>}/>
+          <Route path="/backoffice/members/edit" element={<MembersForm/>} />
+          <Route path="/backoffice/users" element={<UsersList/>} />
+          <Route path="/create-testimonials" element={<TestimonialForm/>} />
+          <Route path="/create-user" element={<UserForm/>} />
+          <Route path="/create-member" element={<MembersForm/>} />
+          <Route path="/create-project" element={<ProjectsForm/>} />
+          <Route path="/school-campaign" element={<SchoolCampaign/>} />
+          <Route path="/toys-campaign" element={<ToysCampaign/>} />
+          <Route path="/about-us" element={<AboutPrincipal/>} />
+          <Route path="/login" element={<LoginForm/>} />
+          <Route path="/backoffice" element={<ScreenDashboardPage/>} />
+          <Route path="/register" element={<Register/>} />
+          <Route path="/backoffice/news" element={<Novedades/>} />
+          <Route path="/donar" element={<Donacion/>} />
+          <Route path="/gracias" element={<Gracias/>} />
+          <Route path="/novedades/:id" element={<DetailNew/>} />
+          <Route path="/activities" element={<Activities/>} />
+          <Route path="/activities/:id" element={<ActivitiesDetail/>} />
+          <Route path="/Novedades" element={<NewsHome/>} />
+          <Route path="/newsletter" element={isLogin() ? <Newsletter /> : <UserNotLogged/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
