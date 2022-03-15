1e99d843fda294cadc979e4ac777a230a0b5bb59
unknown <esteban.abella1@gmail .com>
2022-03-15 20:55:16 -0300

    Task/se agregaron estilos e imagenes

--- src/Components/UI/ReactSocialPlugins/ReactSocialPlugins.js
+++ src/Components/UI/ReactSocialPlugins/ReactSocialPlugins.js
@@ -2,18 +2,19 @@ import React from 'react'
 import { TwitterTweet } from 'react-social-plugins'
 import './stylesReactSocialPlugins.css'
 import ImgTwitter from '../../../ImageProjects/twitter.png'
-import ImgFacebook from '../../../ImageProjects/facebook.png'
+import ImgLinkedin from '../../../ImageProjects/linkedin.png'
 import LazyLoadImages from '../LazyLoadImages/LazyLoadImages'
+import CardLinkedin from './CardLinkedin'
 
 function ReactSocialPlugins(){
 
   return(
     <section className='reactSocialPlugins'>
-        <h1 className='title'>Último Tweet</h1>
+        <h1 className='title'>Nuestras Redes</h1>
 
         <div className='containerSocial'>
 
-            <div className='containerTweet'>
+            <div className='containerRedes'>
                 <TwitterTweet
                   align='left'
                   coversation='none'
@@ -21,11 +22,12 @@ function ReactSocialPlugins(){
                   theme='light'
                   width={325}
                 /> 
+                <CardLinkedin/>
             </div>   
 
             <div className='imgSocial'>
                 <a href='https://www.linkedin.com/in/somos-m%C3%A1s-fundacion-8a1427234/' target='blank'>
-                  <LazyLoadImages src={ImgFacebook} altText='Imagen logo Facebook' classText='ImageSocial'/>
+                  <LazyLoadImages src={ImgLinkedin} altText='Imagen logo Linkedin' classText='ImageSocial'/>
                 </a>
                 <a href='https://twitter.com/SomosMasONGF' target='blank'>
                   <LazyLoadImages src={ImgTwitter} altText='Imagen logo Twitter' classText='ImageSocial'/>
