58984992ed97bc70639f584dab25f484e2f7bc3d
unknown <esteban.abella1@gmail .com>
2022-03-14 22:07:17 -0300

    Task/proteccion ruta donar

--- src/Components/Donations/Donacion.js
+++ src/Components/Donations/Donacion.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './stylesDonacion.css'
+import { useNavigate } from 'react-router-dom'
 
 function Donacion(props) {
+	const navigate = useNavigate()
+
+	useEffect(() => {
+		const isLogin = JSON.parse(localStorage.getItem('token'))
+			? [JSON.parse(localStorage.getItem('token'))]
+			: []
+		if (Object.keys(isLogin).length === 0) {
+			navigate('/')
+		}
+	}, [])
+
 	return (
 		<section className="donacion">
 			<p className="bodyTxt">{props.data}</p>
