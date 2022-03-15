a8f6836ff47e89d9e83653de3ddbe1cbc287f7e5
juliandebra <julian.debra25@gmail.com>
2022-03-15 00:36:55 -0300

    Task/Backoffice Layout

--- src/App.js
+++ src/App.js
@@ -1,36 +1,27 @@
 import './App.css'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import HomeForm from './Components/Home/HomeForm'
 import ActivitiesForm from './Components/Activities/ActivitiesForm/ActivitiesForm'
 import CategoriesForm from './Components/Categories/CategoriesForm'
-import DataScreen from './Components/Testimonials/DataScreen'
 import TestimonialForm from './Components/Testimonials/TestimonialsForm'
 import UserForm from './Components/Users/UsersForm'
 import SchoolCampaign from './Campaigns/School/SchoolCampaign'
 import ToysCampaign from './Campaigns/Toys/ToysCampaign'
-import MemberList from './Components/Members/MemberList'
 import MembersForm from './Components/Members/MembersForm'
 import ProjectsForm from './Components/Projects/ProjectsForm'
 import Activities from './Components/Activities/Actividades'
 import AboutPrincipal from './Components/About/AboutPrincipal'
 import LoginForm from './Components/Auth/LoginForm'
-import OrganizationEditForm from './Components/Home/BackOffice/OrganizationEditForm'
 import ScreenDashboardPage from './Components/ScreenDashboard/ScreenDashboardPage'
 import HomePage from './Components/Home/HomePage'
 import Register from './Components/Auth/RegisterForm'
 import Contact from './Components/Contact/Contact'
 import CreateNews from './Components/News/CreateNews'
 import NewsHome from './Components/News/Home'
-import UsersList from './Components/Users/UsersList/UsersList'
-import Novedades from './Components/Home/BackOffice/Novedades'
 import Donacion from './Components/Donations/Donacion'
 import Gracias from './Components/Donations/Gracias'
 import DetailNew from './Components/News/Detail/DetailNew'
 import ActivitiesDetail from './Components/Activities/Detail/ActivitiesDetail'
-import Categories from './Components/Home/BackOffice/Categories'
-import RedirecSlides from './Components/Slides/RedirecSlides'
-import SlidesList from './Components/Slides/SlidesList'
-import Newsletter from './Components/Newsletter/Newsletter'
+import Newsletter from '../Newsletter/Newsletter'
 import UserNotLogged, { isLogin } from './Components/UI/Errors/UserNotLogged'
 import { contactRestrict } from './Components/Contact/ContactRestrict';
 import userIsAdmin from './Components/UI/Errors/UserIsAdmin'
@@ -46,42 +37,10 @@ function App() {
 					<Route path="/create-category" element={<CategoriesForm />} />
 					<Route path="/create-news" element={<CreateNews />} />
 					<Route
-						path="/backoffice/organization/edit-home"
-						element={userIsAdmin() ? <HomeForm /> : <Navigate to="/" />}
-					/>
-					<Route
-						path="/backoffice/slides/:action"
-						element={userIsAdmin() ? <RedirecSlides /> : <Navigate to="/" />}
-					/>
-					<Route
-						path="/backoffice/slides"
-						element={userIsAdmin() ? <SlidesList /> : <Navigate to="/" />}
-					/>
-					<Route
-						path="/backoffice/organization/edit"
+						path="/backoffice"
 						element={
-							userIsAdmin() ? <OrganizationEditForm /> : <Navigate to="/" />
+							userIsAdmin() ? <ScreenDashboardPage /> : <Navigate to="/" />
 						}
-					/>
-					<Route
-						path="/backoffice/organization"
-						element={userIsAdmin() ? <DataScreen /> : <Navigate to="/" />}
-					/>
-					<Route
-						path="/backoffice/members"
-						element={userIsAdmin() ? <MemberList /> : <Navigate to="/" />}
-					/>
-					<Route
-						path="/backoffice/categories"
-						element={userIsAdmin() ? <Categories /> : <Navigate to="/" />}
-					/>
-					<Route
-						path="/backoffice/members/edit"
-						element={userIsAdmin() ? <MembersForm /> : <Navigate to="/" />}
-					/>
-					<Route
-						path="/backoffice/users"
-						element={userIsAdmin() ? <UsersList /> : <Navigate to="/" />}
 					/>
 					<Route path="/create-testimonials" element={<TestimonialForm />} />
 					<Route path="/create-user" element={<UserForm />} />
@@ -91,17 +50,7 @@ function App() {
 					<Route path="/toys-campaign" element={<ToysCampaign />} />
 					<Route path="/about-us" element={<AboutPrincipal />} />
 					<Route path="/login" element={<LoginForm />} />
-					<Route
-						path="/backoffice"
-						element={
-							userIsAdmin() ? <ScreenDashboardPage /> : <Navigate to="/" />
-						}
-					/>
 					<Route path="/register" element={<Register />} />
-					<Route
-						path="/backoffice/news"
-						element={userIsAdmin() ? <Novedades /> : <Navigate to="/" />}
-					/>
 					<Route path="/donar" element={<Donacion />} />
 					<Route path="/gracias" element={<Gracias />} />
 					<Route path="/novedades/:id" element={<DetailNew />} />
--- src/Components/ScreenDashboard/ScreenDashboardPage.js
+++ src/Components/ScreenDashboard/ScreenDashboardPage.js
@@ -1,12 +1,73 @@
 import React from 'react'
 import './stylesScreenDashboardPage.css'
+import HeaderComponent from '../Home/BackOffice/HeaderComponent'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { userIsAdmin } from '../../../utils/user'
+import HomeForm from '../Home/BackOffice/HomeForm'
+import RedirecSlides from '../Slides/RedirecSlides'
+import SlidesList from '../Slides/SlidesList'
+import OrganizationEditForm from '../Home/BackOffice/OrganizationEditForm'
+import DataScreen from '../Testimonials/DataScreen'
+import MemberList from '../Members/MemberList'
+import Categories from '../Home/BackOffice/Categories'
+import MembersForm from '../Members/MembersForm'
+import UsersList from '../Users/UsersList/UsersList'
+import UserNotLogged, { isLogin } from '../Users/UserNotLogged'
+import Novedades from '../Home/BackOffice/Novedades'
 
-function ScreenDashboardPage() {
-	return (
-		<main className="screenDashboardPage">
-			<h2>Bienvenido</h2>
+const ScreenDashboardPage = () => {
+  	return (
+		<main className='screenDashboardPage' >
+			<HeaderComponent/>
+			{/* Sidebar */}
+			<Routes>
+				<Route
+					path="/backoffice/organization/edit-home"
+					element={userIsAdmin() ? <HomeForm /> : <Navigate to="/" />}
+				/>
+				<Route
+					path="/backoffice/slides/:action"
+					element={userIsAdmin() ? <RedirecSlides /> : <Navigate to="/" />}
+				/>
+				<Route
+					path="/backoffice/slides"
+					element={userIsAdmin() ? <SlidesList /> : <Navigate to="/" />}
+				/>
+				<Route
+					path="/backoffice/organization/edit"
+					element={
+						userIsAdmin() ? <OrganizationEditForm /> : <Navigate to="/" />
+					}
+				/>
+				<Route
+					path="/backoffice/organization"
+					element={userIsAdmin() ? <DataScreen /> : <Navigate to="/" />}
+				/>
+				<Route
+					path="/backoffice/members"
+					element={userIsAdmin() ? <MemberList /> : <Navigate to="/" />}
+				/>
+				<Route
+					path="/backoffice/categories"
+					element={userIsAdmin() ? <Categories /> : <Navigate to="/" />}
+				/>
+				<Route
+					path="/backoffice/members/edit"
+					element={userIsAdmin() ? <MembersForm /> : <Navigate to="/" />}
+				/>
+				<Route
+					path="/backoffice/users"
+					element={userIsAdmin() ? <UsersList /> : <Navigate to="/" />}
+				/>
+				<Route
+					path="/backoffice/news"
+					element={userIsAdmin() ? <Novedades /> : <Navigate to="/" />}
+				/>
+			</Routes>
 		</main>
-	)
+  )
 }
 
 export default ScreenDashboardPage
+
+
