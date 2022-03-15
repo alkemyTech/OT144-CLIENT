c0d9f2f0a6ed643ee725b85e7f71bd817c2542b3
Julián Debrabandere <64337359+juliandebra@users.noreply.github.com>
2022-03-14 22:07:14 -0300

    feat: contact restriction
    * Pull merge
    
    * Main conflicts
    
    * Resolve conflicts
    
    * Resolve conflicts
    
    * Task/Contact restriction for admin
    
    * Novedades.js & yarn.lock reseted
    
    Co-authored-by: Federico Miranda <federico.mirandaa@gmail.com>

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ import RedirecSlides from './Components/Slides/RedirecSlides'
 import SlidesList from './Components/Slides/SlidesList'
 import Newsletter from './Components/Newsletter/Newsletter'
 import UserNotLogged, { isLogin } from './Components/UI/Errors/UserNotLogged'
+import { contactRestrict } from './Components/Contact/ContactRestrict';
 import userIsAdmin from './Components/UI/Errors/UserIsAdmin'
 
 function App() {
@@ -40,7 +41,7 @@ function App() {
 			<BrowserRouter>
 				<Routes>
 					<Route path="/" exact element={<HomePage />} />
-					<Route path="/contact" element={<Contact />} />
+					<Route path="/contact" element={ contactRestrict() ? <Contact/> : <HomePage/>} />
 					<Route path="/create-activity" element={<ActivitiesForm />} />
 					<Route path="/create-category" element={<CategoriesForm />} />
 					<Route path="/create-news" element={<CreateNews />} />
--- src/Components/Home/BackOffice/Novedades.js
+++ src/Components/Home/BackOffice/Novedades.js
@@ -168,4 +168,3 @@ function Novedades() {
 }
 
 export default Novedades
-
