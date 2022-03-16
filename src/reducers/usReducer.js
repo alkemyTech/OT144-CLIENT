a2de4ffeaafcf8f54426710b65f1627b5954e0e6
cesar londoño <slac_@hotmail.com>
2022-03-16 07:49:34 -0300

    refactorizacion 003

--- src/reducers/usReducer.js
+++ src/reducers/usReducer.js
@@ -7,13 +7,13 @@ const initialState = {
 
 export const usReducer = (state = initialState, action) => {
 	switch (action.type) {
-		case types.ABOUTUS_UPDATE:
-		case types.ABOUTUS_ADD:
-			return {
-				...state,
-			}
+	case types.ABOUTUS_UPDATE:
+	case types.ABOUTUS_ADD:
+		return {
+			...state,
+		}
 
-		default:
-			return state
+	default:
+		return state
 	}
 }
