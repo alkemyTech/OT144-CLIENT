497f844199e799ea887e02d40399c19df59521e6
Mariel Romeo <marielceciliaromeo@hotmail.com>
2022-03-15 21:47:24 -0300

    [ADD]error in testimonialsForm

--- src/Components/Testimonials/TestimonialsForm.js
+++ src/Components/Testimonials/TestimonialsForm.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react'
+import { createTestimonials } from '../../Services/TestimonialsApiService'
+import BasicAlert from '../UI/Alerts/BasicAlert'
+import ErrorAlert from '../UI/Alerts/ErrorAlert'
 import '../FormStyles.css'
 
 const TestimonialForm = () => {
 	const [initialValues, setInitialValues] = useState({
 		name: '',
 		description: '',
+		image: '',
 	})
 
 	const handleChange = (e) => {
@@ -12,12 +16,26 @@ const TestimonialForm = () => {
 			setInitialValues({ ...initialValues, name: e.target.value })
 		} else if (e.target.name === 'description') {
 			setInitialValues({ ...initialValues, description: e.target.value })
+		} else if (e.target.name === 'image') {
+			setInitialValues({ ...initialValues, image: e.target.value })
 		}
 	}
 
-	const handleSubmit = (e) => {
+	const handleSubmit = async (e) => {
 		e.preventDefault()
-		console.log(initialValues)
+		try {
+			const response = await createTestimonials(initialValues)
+			if (response.status === 200) {
+				BasicAlert(
+					'success',
+					'Mensaje enviado!',
+					'Estaremos en contacto en la proximidad',
+					1500
+				)
+			}
+		} catch (error) {
+			return <ErrorAlert />
+		}
 	}
 
 	return (
@@ -36,7 +54,15 @@ const TestimonialForm = () => {
 				name="description"
 				value={initialValues.description}
 				onChange={handleChange}
-				placeholder="Testimonial description"
+				placeholder="Testimonial Description"
+			></input>
+			<input
+				className="input-field"
+				type="text"
+				name="image"
+				value={initialValues.image}
+				onChange={handleChange}
+				placeholder="Testimonial Image"
 			></input>
 			<button className="submit-btn" type="submit">
 				Send
