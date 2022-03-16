71a3f28ff8efde7242ddb3bfacd21fa033845aad
Braian Fernandez <62818757+Braifz@users.noreply.github.com>
2022-03-16 00:25:47 -0300

    Update src/Components/Layout/LayoutPublic.js
    Co-authored-by: Federico Miranda <federico.mirandaa@gmail.com>

--- src/Components/Layout/LayoutPublic.js
+++ src/Components/Layout/LayoutPublic.js
@@ -4,11 +4,11 @@ import './LayoutPublic.css'
 
 const LayoutPublic = ({ children }) => {
 	return (
-		<div>
+		<>
 			<Header />
 			<div className="layout-container">{children}</div>
 			<Footer />
-		</div>
+		<>
 	)
 }
 
