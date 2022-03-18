3c427e54316a702b59d3078ba24a6daff74b4607
Marlon Gómez <marlonjulian15@gmail.com>
2022-03-18 17:27:17 -0500

    Add Inputs in ProjectsForm

--- src/Components/Projects/ProjectsForm.js
+++ src/Components/Projects/ProjectsForm.js
@@ -1,47 +1,82 @@
 import React, { useState } from 'react'
+import { Formik, Form, Field } from 'formik'
 import '../FormStyles.css'
+import { MIN_LENGTH_TITLE_PROJECTS, TEXT_INPUT_REQUIRED } from '../../constants'
 
+const validate = (values) => {
+	const errors = {}
+	if (!values.title) {
+		errors.title = TEXT_INPUT_REQUIRED
+	} else if (values.title.length < MIN_LENGTH_TITLE_PROJECTS) {
+		errors.title = `El título debe tener al menos ${MIN_LENGTH_TITLE_PROJECTS} caracteres`
+	}
+	return errors
+}
 const ProjectsForm = () => {
-	const [initialValues, setInitialValues] = useState({
+	const [formValues] = useState({
 		title: '',
 		description: '',
+		image: '',
+		due_date: '',
 	})
-
-	const handleChange = (e) => {
-		if (e.target.name === 'title') {
-			setInitialValues({ ...initialValues, title: e.target.value })
-		} else if (e.target.name === 'description') {
-			setInitialValues({ ...initialValues, description: e.target.value })
-		}
-	}
 
 	const handleSubmit = (e) => {
 		e.preventDefault()
-		console.log(initialValues)
+		console.log(formValues)
 	}
 
 	return (
-		<form className="form-container" onSubmit={handleSubmit}>
-			<input
-				className="input-field"
-				type="text"
-				name="title"
-				value={initialValues.title}
-				onChange={handleChange}
-				placeholder="Title"
-			></input>
-			<input
-				className="input-field"
-				type="text"
-				name="description"
-				value={initialValues.description}
-				onChange={handleChange}
-				placeholder="Write some description"
-			></input>
-			<button className="submit-btn" type="submit">
-				Send
-			</button>
-		</form>
+		<Formik
+			initialValues={formValues}
+			validate={validate}
+			onSubmit={handleSubmit}
+		>
+			{({ errors, handleBlur, touched, handleChange }) => (
+				<Form>
+					<Field
+						className="input-field"
+						type="text"
+						name="title"
+						placeholder="Título del Proyecto"
+						onBlur={handleBlur}
+					/>
+					{touched.title && errors.title ? (
+						<div className="alert-danger">{errors.title}</div>
+					) : null}
+
+					<Field
+						className="input-field"
+						type="text"
+						name="description"
+						placeholder="Escribe una descripción"
+						onBlur={handleBlur}
+					/>
+					{touched.description && errors.description ? (
+						<div className="alert-danger">{errors.description}</div>
+					) : null}
+
+					<input
+						type="file"
+						name="image"
+						accept="image/*"
+						onBlur={handleBlur}
+						onChange={handleChange}
+					/>
+					{touched.image && errors.image ? (
+						<div className="alert-danger">{errors.image}</div>
+					) : null}
+
+					<input type="date" name="due_date" onBlur={handleBlur} />
+					{touched.due_date && errors.due_date ? (
+						<div className="alert-danger">{errors.due_date}</div>
+					) : null}
+
+					<button className="submit-btn" type="submit">
+						Crear
+					</button>
+				</Form>
+			)}
+		</Formik>
 	)
 }
 
