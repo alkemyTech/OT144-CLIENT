3aeb5d85dff0b654be78797c6f8c5d6f88c0eef3
Mariel Romeo <marielceciliaromeo@hotmail.com>
2022-03-21 20:42:49 -0300

    [ADD] documentation in RegisterForm.test.js

--- src/Components/Auth/RegisterForm.test.js
+++ src/Components/Auth/RegisterForm.test.js
@@ -10,6 +10,17 @@ import {
 } from '@testing-library/react'
 import { createUser, deleteUser } from '../../Services/userService'
 
+/**
+ * Se creo un test que valide que no se haga submit si no está correctamente completo. Además, debe mostrar el mensaje de error al usuario.
+ * El mismo recibe los datos de nombre, apellido, email, contraseña y confirmar contraseña.
+ * En este caso, se envía el valor de confirmar contraseña de manera incorrecta (no es igual a la contraseña).
+ * Lo que se espera es que el submit no sea llamado si el formulario tiene errores y al mismo tiempo, que aparezca el error en el input de
+ * confirmación de contraseña.
+ *
+ * El test pasa correctamente.
+ * Ejecutado el 21/03/2022
+ */
+
 test('RegisterForm shouldn´t be submitted if it is not correctly completed. Passwords are not the same. It should render an error message', () => {
 	cleanup()
 	const onSubmit = jest.fn()
@@ -44,7 +55,18 @@ test('RegisterForm shouldn´t be submitted if it is not correctly completed. Pas
 	})
 })
 
-// Hay que actualizar el email cada vez que se ejecuta porque sino no se hace la peticion porque el mail figura en la base de datos a pesar de que lo borro
+/**
+ * Se creo un test que valide que si los campos están completos correctamente, haga una petición post y cree un usuario.
+ * El mismo recibe los datos de nombre, apellido, email, contraseña y confirmar contraseña de manera correcta.
+ * Lo que se espera es que se haga la petición y que la respuesta sea un objeto que devuelva la data con el nombre y el mail, el mensaje de usuario
+ * guardado correctamente y la propiedad success en true.
+ * Luego se borra el usuario creado con el id de usuario que trae la respuesta para que no quede en la base de datos.
+ *
+ * Si el test se ejecuta nuevamente, hay que cambiar el mail ya que la api retorna un error de que el usuario ya existe, a pesar de que fue borrado.
+ *
+ * El test pasa correctamente.
+ * Ejecutado el 21/03/2022
+ */
 
 test('RegisterForm should do a post petition and create an user', async () => {
 	cleanup()
@@ -67,6 +89,16 @@ test('RegisterForm should do a post petition and create an user', async () => {
 
 	await deleteUser(user.data.id)
 })
+
+/**
+ * Se creo un test que valide que se haga una petición post y que retorne un error si el mail ya existe en la base de datos.
+ * El mismo recibe los datos de nombre, apellido, email, contraseña y confirmar contraseña de manera correcta pero el email que recibe es de un
+ * usuario ya creado.
+ * Lo que se espera es que se haga la petición y que la respuesta sea un objeto que devuelva el mensaje de que la data enviada es inválida.
+ *
+ * El test pasa correctamente.
+ * Ejecutado el 21/03/2022
+ */
 
 test('RegisterForm should do a post petition and return an error because the email exists in the data base', async () => {
 	const user = await createUser({
