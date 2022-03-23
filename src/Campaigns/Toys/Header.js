b6b6cfe06210bab283d177106076e03f5ed73f13
Julián Debrabandere <64337359+juliandebra@users.noreply.github.com>
2022-03-23 20:46:18 -0300

    feat: campaings header
    * Task/Search users by Role
    
    * Revert main commit
    
    * Task/Responsiv-Header

--- src/Campaigns/Toys/Header.js
+++ src/Campaigns/Toys/Header.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import LogoJuguetes from '../../ImageProjects/Logo-juguetes.png'
+import LogoONG from '../../ImageProjects/SomosMas.png'
+import './Header.css'
 
 const Header = () => {
-	return <div></div>
+	return (
+		<div className="header-container">
+			<img src={LogoONG} alt="Logo ONG" className="logoONG" />
+			<img src={LogoJuguetes} alt="Logo juguetes" className="logoJuguetes" />
+			<h1 className="slogan">JUGUETES POR MÁS SONRISAS</h1>
+		</div>
+	)
 }
 
 export default Header
