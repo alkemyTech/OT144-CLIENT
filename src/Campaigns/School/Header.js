cb92ea810110c7944d570965baa8b1d94535a386
juliandebra <julian.debra25@gmail.com>
2022-03-23 18:53:38 -0300

    Task/Responsiv-Header

--- src/Campaigns/School/Header.js
+++ src/Campaigns/School/Header.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import LogoEscuela from '../../ImageProjects/Logo-escolares.png'
+import LogoONG from '../../ImageProjects/SomosMas.png'
+import './Header.css'
 
 const Header = () => {
-	return <div></div>
+	return (
+		<div className="header-container">
+			<img src={LogoONG} alt="Logo ONG" className="logoONG" />
+			<img src={LogoEscuela} alt="Logo escuela" className="logoEscuela" />
+			<h1 className="slogan">JUNTOS EN LA VUELTA AL COLE</h1>
+		</div>
+	)
 }
 
 export default Header
