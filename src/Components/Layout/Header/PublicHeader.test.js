bc733cb7d94042fd69dd0353c6b43e7b0d71d7d7
cesar londoño <slac_@hotmail.com>
2022-03-23 07:41:52 -0300

    0003  test

--- src/Components/Layout/Header/PublicHeader.test.js
+++ src/Components/Layout/Header/PublicHeader.test.js
@@ -7,22 +7,20 @@ import { isLogin } from '../../UI/Errors/UserNotLogged'
 
 describe('test in <PublicHeader />', () => {
 	test('should show public links, login and registration if user not authenticated', () => {
-		// const wrapper = render(
-		// 	<BrowserRouter>
-		// 		<PublicHeader />
-		// 	</BrowserRouter>
-		// )
-		// wrapper.debug()
 		render(
 			<BrowserRouter>
 				<PublicHeader />
 			</BrowserRouter>
 		)
-		console.log(isLogin())
-		expect(screen.getByTestId('button-register')).toBeInTheDocument()
-		expect(screen.getByTestId('button-login')).toBeInTheDocument()
-		expect(screen.getByTestId('button-register').textContent).toBe('Registrate')
-		expect(screen.getByTestId('button-login').textContent).toBe('Inicia sesión')
+		const isLoginTest = isLogin()
+		const register = screen.getByTestId('button-register')
+		const login = screen.getByTestId('button-login')
+
+		expect(isLoginTest).not.toBe(false)
+		expect(register).toBeInTheDocument()
+		expect(login).toBeInTheDocument()
+		expect(register.textContent).toBe('Registrate')
+		expect(login.textContent).toBe('Inicia sesión')
 	})
 	test('should not show public links, login and registration if user authenticated', () => {
 		render(
@@ -30,6 +28,15 @@ describe('test in <PublicHeader />', () => {
 				<PublicHeader />
 			</BrowserRouter>
 		)
+		const isLoginTest = isLogin()
+		const register = screen.getByTestId('button-register')
+		const login = screen.getByTestId('button-login')
+
+		expect(isLoginTest).not.toBe(true)
+		expect(register).not.toBeInTheDocument()
+		expect(login).not.toBeInTheDocument()
+		expect(register.textContent).not.toBe('Registrate')
+		expect(login.textContent).not.toBe('Inicia sesión')
 	})
 	// test('should not show public links, login and registration if user authenticated', () => {
 
