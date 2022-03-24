81ba11654ccb4fa329c4c66a09979b903c4b3c5c
Braifz <braian94lp@gmail.com>
2022-03-24 17:22:16 -0300

    fix error img

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import ContactForm from './ContactForm'
 import './Contact.css'
-import TitleComponent from '../title/TitleComponent'
 import { getContact } from '../../Services/ServiceAPIContact'
 import ErrorAlert from '../UI/Alerts/ErrorAlert'
+import LayoutPublic from '../Layout/LayoutPublic'
 
 const Contact = ({ img, name, phone, email }) => {
 	const [error, setError] = React.useState(false)
@@ -21,28 +21,12 @@ const Contact = ({ img, name, phone, email }) => {
 	}
 
 	return (
-		<div className="contact-container">
-			<TitleComponent title="Contacto" img={img} />
-
-			<ul className="contact-list">
-				<li>
-					{' '}
-					Nombre:<span> {name} </span>{' '}
-				</li>
-				<li>
-					{' '}
-					Email: <span> {email} </span>
-				</li>
-				<li>
-					{' '}
-					Phone: <span> {phone} </span>{' '}
-				</li>
-			</ul>
-
-			<h3 className="subtitle">Contactanos</h3>
-
-			<ContactForm />
-		</div>
+		<LayoutPublic>
+			<div className="contact-container">
+				<h3 className="subtitle">Contactanos</h3>
+				<ContactForm />
+			</div>
+		</LayoutPublic>
 	)
 }
 
