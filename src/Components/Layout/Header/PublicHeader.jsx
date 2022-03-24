1af128b5ac2bef11cd20091f344d8fa72c21c783
Marlon Gómez <marlonjulian15@gmail.com>
2022-03-24 11:11:23 -0500

    Use SideBar in PublicHeader

--- src/Components/Layout/Header/PublicHeader.jsx
+++ src/Components/Layout/Header/PublicHeader.jsx
@@ -1,8 +1,11 @@
-import React, { useState, useEffect } from "react"
-import { Link } from "react-router-dom"
-import Logo from "./LOGO-SOMOS MAS.png"
-import "./PublicHeader.css"
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import Logo from './LOGO-SOMOS MAS.png'
+import SideBarPublic from '../../Home/SideBarPublic.js'
+import './PublicHeader.css'
+import btnMenuImg from '../../../ImageProject/btn-menu.png'
 import { isLogin } from '../../../Components/UI/Errors/UserNotLogged'
+import useWindowDimensions from '../../../hooks/useWindowDimensions.js'
 
 export default function PublicHeader() {
 	const datos = [
@@ -15,50 +18,69 @@ export default function PublicHeader() {
 		{ nombre: 'Contribuye', url: '/donar' },
 	]
 
-	const [activePath, setActivePath] = useState("")
+	const [activePath, setActivePath] = useState('')
 	const [isLoginRegister, setIsLoginRegister] = useState(false)
+
+	const [btnResponsive, setBtnResponsive] = useState(false)
+
+	const { width } = useWindowDimensions()
+
+	const handleClickBtn = () => {
+		if (btnResponsive === false) {
+			setBtnResponsive(true)
+		} else {
+			setBtnResponsive(false)
+		}
+	}
 
 	useEffect(() => {
 		setActivePath(window.location.pathname)
 	}, [activePath])
 
-	useEffect(
-		() => {
-			if (isLogin === true) {
-				setIsLoginRegister(true)
-			}
-		},
-		[]
-	)
+	useEffect(() => {
+		if (isLogin === true) {
+			setIsLoginRegister(true)
+		}
+	}, [])
 
 	return (
-		<header className="containerHeader">
+		<header className={`containerHeader ${width <= 842 ? 'reverse' : ''}`}>
 			<div className="containerLogo">
 				<img src={Logo} alt="logo" />
 			</div>
-			<nav className="containerHeaderNav">
-				<ul>
-
-					{datos.map((dato, index) => (
-
-						<li
-							key={`${dato.nombre}${index}`}
-							id={dato.url === activePath ? 'activePath' : null}
-						>
-							<Link to={dato.url}>{dato.nombre}</Link>
-						</li>
-
-					))}
-				</ul>
-			</nav>
-			<div className="containerHeaderButtons">
-				{!isLoginRegister &&
-					<>
-						<Link data-testid='button-register' to="/register"><button>Registrate</button></Link>
-						<Link data-testid='button-login' to="/login"><button>Inicia sesión</button></Link>
-					</>
-				}
-			</div>
+			<SideBarPublic isOpen={btnResponsive} />
+			{width > 842 ? (
+				<>
+					<nav className="containerHeaderNav">
+						<ul>
+							{datos.map((dato, index) => (
+								<li
+									key={`${dato.nombre}${index}`}
+									id={dato.url === activePath ? 'activePath' : null}
+								>
+									<Link to={dato.url}>{dato.nombre}</Link>
+								</li>
+							))}
+						</ul>
+					</nav>
+					<div className="containerHeaderButtons">
+						{!isLoginRegister && (
+							<>
+								<Link data-testid="button-register" to="/register">
+									<button>Registrate</button>
+								</Link>
+								<Link data-testid="button-login" to="/login">
+									<button>Inicia sesión</button>
+								</Link>
+							</>
+						)}
+					</div>
+				</>
+			) : (
+				<div className="menu-btn" onClick={handleClickBtn}>
+					<img src={btnMenuImg} alt="Imagen Btn Menu"></img>
+				</div>
+			)}
 		</header>
 	)
 }
