8bf0f278a52b7e4a37c5ebc21617b94c728566b7
Esteban Abella <75279487+EstebanAbella@users.noreply.github.com>
2022-03-24 18:22:02 -0300

    fix: activities table
    * Arreglo componente ActivitiesTable
    
    * Se arreglo estilo tabla
    
    Co-authored-by: unknown <esteban.abella1@gmail .com>

--- src/actions/Activities.js
+++ src/actions/Activities.js
@@ -5,6 +5,5 @@ export const startGetActivities =
 	(search = '') =>
 		async (dispatch) => {
 			const response = await getActividades(search)
-			console.log(response)
 			dispatch(setActivitiesAction(response.data.data))
 		}
