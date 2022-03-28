df1167e6dceb7a03b1a32bc1c1d3da841984fc70
Marlon Gómez <marlonjulian15@gmail.com>
2022-03-28 12:13:00 -0500

    Rendering HTML in DetailContent component

--- src/Components/News/Detail/DetailContent.js
+++ src/Components/News/Detail/DetailContent.js
@@ -1,32 +1,13 @@
-import React, { useRef, useLayoutEffect } from 'react'
+import React from 'react'
 import './stylesDetailNew.css'
 
 const DetailContent = ({ data }) => {
-	const containerRef = useRef()
-
-	useLayoutEffect(() => {
-		if (containerRef.current) {
-			const nodes = containerRef.current.childNodes
-			for (const node of nodes) {
-				if (node.nodeName === 'P' && node.innerHTML !== '&nbsp;') {
-					node.classList.add('paragraph')
-				}
-				if (node.nodeName === 'H2') {
-					node.classList.add('title')
-				}
-				if (node.nodeName === 'H3') {
-					node.classList.add('subtitle')
-				}
-			}
-		}
-	}, [])
-
-	console.log(data)
 	return (
 		<div className="detailContentContainer">
-			<div ref={containerRef} className="container-new detail">
-				<p className="contentDetail">{data.content}</p>
-			</div>
+			<div
+				className="container-new detail"
+				dangerouslySetInnerHTML={{ __html: data.content }}
+			></div>
 		</div>
 	)
 }
